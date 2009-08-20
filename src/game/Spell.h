832db8eda524c18b9b3f7f37c8673eb704bf0093
krz <devnull@localhost>
2009-08-20 15:33:31 +0200

    typo...

--- src/game/Spell.h
+++ src/game/Spell.h
@@ -676,7 +676,7 @@ namespace Trinity
                             i_data->push_back(itr->getSource());
                         break;
                     default:
-                        if(PUSH_SRC_CENTER && i_caster) // if caster then check distance from caster to target (because of model collision)
+                        if(i_push_type == PUSH_SRC_CENTER && i_caster) // if caster then check distance from caster to target (because of model collision)
                         {
                             if(i_caster->IsWithinDistInMap( itr->getSource(), i_radius) )
                                 i_data->push_back(itr->getSource());
