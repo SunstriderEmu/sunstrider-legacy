51e528fdfd1d2cf429f991cbce53508244761290
DrThum <DrThum@users.noreply.github.com>
2009-10-23 11:48:00 +0200

    [TC1 1746] Add save interface to Shadowfang Keep instance. Now doors are opened even when players logout. Instance Working properly now.

--- src/bindings/scripts/scripts/zone/shadowfang_keep/instance_shadowfang_keep.cpp
+++ src/bindings/scripts/scripts/zone/shadowfang_keep/instance_shadowfang_keep.cpp
@@ -16,8 +16,8 @@
 
 /* ScriptData
 SDName: Instance_Shadowfang_Keep
-SD%Complete: 75
-SDComment: TODO: check what other parts would require additional code (ex: make sure door are in open state if boss dead)
+SD%Complete: 100
+SDComment: Complete, Doors checked
 SDCategory: Shadowfang Keep
 EndScriptData */
 
@@ -30,7 +30,7 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
 {
     instance_shadowfang_keep(Map *map) : ScriptedInstance(map) {Initialize();};
 
-    uint32 Encounter[ENCOUNTERS];
+    uint32 Encounters[ENCOUNTERS];
     std::string str_data;
 
     uint64 DoorCourtyardGUID;
@@ -44,47 +44,21 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
         DoorArugalGUID = 0;
 
          for(uint8 i=0; i < ENCOUNTERS; ++i)
-             Encounter[i] = NOT_STARTED;
-    }
-
-    Player* GetPlayerInMap()
-    {
-        Map::PlayerList const& players = instance->GetPlayers();
-
-        if (!players.isEmpty())
-        {
-            for(Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
-            {
-                if (Player* plr = itr->getSource())
-                    return plr;
-            }
-        }
-        debug_log("TSCR: Instance Shadowfang Keep: GetPlayerInMap, but PlayerList is empty!");
-        return NULL;
+             Encounters[i] = NOT_STARTED;
     }
 
     void OnObjectCreate(GameObject *go)
     {
         switch(go->GetEntry())
         {
-        case 18895: DoorCourtyardGUID = go->GetGUID(); break;
-        case 18972: DoorSorcererGUID = go->GetGUID(); break;
-        case 18971: DoorArugalGUID = go->GetGUID(); break;
+        case 18895: DoorCourtyardGUID = go->GetGUID();
+            if(Encounters[0] == DONE) HandleGameObject(NULL,true,go); break;
+        case 18972: DoorSorcererGUID = go->GetGUID(); 
+            if(Encounters[2] == DONE) HandleGameObject(NULL,true,go); break;
+        case 18971: DoorArugalGUID = go->GetGUID(); 
+            if(Encounters[3] == DONE) HandleGameObject(NULL,true,go); break;
+
         }
-    }
-
-    void HandleGameObject(uint64 guid, uint32 state)
-    {
-        Player *player = GetPlayerInMap();
-
-        if (!player || !guid)
-        {
-            debug_log("TSCR: Instance Shadowfang Keep: HandleGameObject fail");
-            return;
-        }
-
-        if (GameObject *go = GameObject::GetGameObject(*player,guid))
-            go->SetGoState(state);
     }
 
     void SetData(uint32 type, uint32 data)
@@ -93,21 +67,21 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
         {
             case TYPE_FREE_NPC:
                 if(data == DONE)
-                    HandleGameObject(DoorCourtyardGUID,0);
-                Encounter[0] = data;
+                    HandleGameObject(DoorCourtyardGUID,true);
+                Encounters[0] = data;
                 break;
             case TYPE_RETHILGORE:
-                Encounter[1] = data;
+                Encounters[1] = data;
                 break;
             case TYPE_FENRUS:
                 if(data == DONE)
-                    HandleGameObject(DoorSorcererGUID,0);
-                Encounter[2] = data;
+                    HandleGameObject(DoorSorcererGUID,true);
+                Encounters[2] = data;
                 break;
             case TYPE_NANDOS:
                 if(data == DONE)
-                    HandleGameObject(DoorArugalGUID,0);
-                Encounter[3] = data;
+                    HandleGameObject(DoorArugalGUID,true);
+                Encounters[3] = data;
                 break;
         }
 
@@ -116,7 +90,7 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
             OUT_SAVE_INST_DATA;
 
             std::ostringstream saveStream;
-            saveStream << Encounter[0] << " " << Encounter[1] << " " << Encounter[2] << " " << Encounter[3];
+            saveStream << Encounters[0] << " " << Encounters[1] << " " << Encounters[2] << " " << Encounters[3];
 
             str_data = saveStream.str();
 
@@ -130,13 +104,13 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
         switch(type)
         {
             case TYPE_FREE_NPC:
-                return Encounter[0];
+                return Encounters[0];
             case TYPE_RETHILGORE:
-                return Encounter[1];
+                return Encounters[1];
             case TYPE_FENRUS:
-                return Encounter[2];
+                return Encounters[2];
             case TYPE_NANDOS:
-                return Encounter[3];
+                return Encounters[3];
         }
         return 0;
     }
@@ -157,11 +131,14 @@ struct TRINITY_DLL_DECL instance_shadowfang_keep : public ScriptedInstance
         OUT_LOAD_INST_DATA(in);
 
         std::istringstream loadStream(in);
-        loadStream >> Encounter[0] >> Encounter[1] >> Encounter[2] >> Encounter[3];
+        loadStream >> Encounters[0] >> Encounters[1] >> Encounters[2] >> Encounters[3];
 
         for(uint8 i = 0; i < ENCOUNTERS; ++i)
-            if (Encounter[i] == IN_PROGRESS)
-                Encounter[i] = NOT_STARTED;
+        {
+            if (Encounters[i] == IN_PROGRESS)
+                Encounters[i] = NOT_STARTED;
+
+        }
 
         OUT_LOAD_INST_DATA_COMPLETE;
     }
