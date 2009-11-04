603a414ec88067aa923158d2738c1fd4552d136e
DrThum <DrThum@users.noreply.github.com>
2009-11-04 15:35:08 +0100

    Fix a crash in Ruthlessness fix

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18200,7 +18200,8 @@ void Player::ClearComboPoints()
     {
         uint32 procChance = urand(1,100);
         if ( (HasSpell(14161) && procChance <= 60) || (HasSpell(14160) && procChance <= 40) || (HasSpell(14156) && procChance <= 20) )
-            AddComboPoints(this->getVictim(), 1);
+            if (this->getVictim())
+                AddComboPoints(this->getVictim(), 1);
     }
 }
 
