b319573c03c063ee310cfa5661bf29a74b1e20d5
DrThum <DrThum@users.noreply.github.com>
2009-11-24 01:56:24 +0100

    Fix quest 10190, but there's probably a better way to handle it.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2307,6 +2307,21 @@ void Spell::EffectApplyAura(uint32 i)
             sLog.outDebug("Spell: Additional Aura is: %u", AdditionalSpellInfo->EffectApplyAuraName[0]);
         }
     }
+    
+    if (m_spellInfo->Id == 34219)                                 // Quest 10190
+    {
+        if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT || m_caster->GetTypeId() != TYPEID_PLAYER)
+            return;
+            
+        Creature* cTarget = (Creature*)unitTarget;
+        
+        if (cTarget->GetHealth() <= cTarget->GetMaxHealth()/5)
+        {
+            ((Player*)m_caster)->KilledMonster(19595, 0);
+            cTarget->DealDamage(cTarget, cTarget->GetMaxHealth(), NULL, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NONE, NULL, false);
+            cTarget->RemoveCorpse();
+        }
+    }
 
     // Prayer of Mending (jump animation), we need formal caster instead original for correct animation
     if( m_spellInfo->SpellFamilyName == SPELLFAMILY_PRIEST && (m_spellInfo->SpellFamilyFlags & 0x00002000000000LL))
