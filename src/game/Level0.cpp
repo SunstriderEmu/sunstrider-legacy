af02f010a31f49494a3e9bf7d911d576a41754c9
DrThum <DrThum@users.noreply.github.com>
2010-04-20 08:45:07 +0200

    Blocked .start command if player is in battleground or arena

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -81,6 +81,13 @@ bool ChatHandler::HandleStartCommand(const char* /*args*/)
         SetSentErrorMessage(true);
         return false;
     }
+    
+    if(chr->InBattleGround())
+    {
+        SendSysMessage(LANG_YOU_IN_BATTLEGROUND);
+        SetSentErrorMessage(true);
+        return false;
+    }
 
     // cast spell Stuck
     //chr->CastSpell(chr,7355,false);
