c71478dc5450c7294d8258a08b92d024db358ded
DrThum <DrThum@users.noreply.github.com>
2010-06-15 14:03:00 +0200

    Fix for item duplication from OregonCore

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -1544,6 +1544,12 @@ void ObjectMgr::LoadItemPrototypes()
             sLog.outErrorDb("Item (Entry: %u) has too large value in stackable (%u), replace by hardcoded upper limit (255).",i,proto->Stackable);
             const_cast<ItemPrototype*>(proto)->Stackable = 255;
         }
+        
+        if (proto->ContainerSlots > MAX_BAG_SIZE)
+        {
+            sLog.outErrorDb("Item (Entry: %u) has too large value in ContainerSlots (%u), replace by hardcoded limit (%u).",i,proto->ContainerSlots,MAX_BAG_SIZE);
+            const_cast<ItemPrototype*>(proto)->ContainerSlots = MAX_BAG_SIZE;
+        }
 
         for (int j = 0; j < 10; j++)
         {
