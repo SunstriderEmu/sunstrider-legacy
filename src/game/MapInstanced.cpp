e927b379acce5d435879c4ff9d5fc11ee0fb7a55
target0 <dav.lebrun@gmail.com>
2010-06-20 21:50:48 +0200

    Fix gameobject handling in MapInstanced::GetInstance

--- src/game/MapInstanced.cpp
+++ src/game/MapInstanced.cpp
@@ -112,7 +112,7 @@ void MapInstanced::UnloadAll()
 */
 Map* MapInstanced::GetInstance(const WorldObject* obj)
 {
-    if (obj->GetTypeId() == TYPEID_UNIT)
+    if (obj->GetTypeId() == TYPEID_UNIT || obj->GetTypeId() == TYPEID_GAMEOBJECT)
     {
         assert(obj->GetMapId() == GetId() && obj->GetInstanceId());
         return _FindMap(obj->GetInstanceId());
