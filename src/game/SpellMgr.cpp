fb9a24d3524e683565571717cb7c53f7a161dc37
DrThum <DrThum@users.noreply.github.com>
2010-08-22 23:09:29 +0200

    Siphon Soul (Malacrass) is a debuff

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -571,6 +571,7 @@ bool IsPositiveEffect(uint32 spellId, uint32 effIndex)
         case 34877:                                         // Custodian of Time
         case 34700:                                         // Allergic Reaction
         case 31719:                                         // Suspension
+        case 43501:                                         // Siphon Soul (Malacrass)
             return false;
     }
 
