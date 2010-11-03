c29119c189e8ad41000bba8d133e14db96185828
DrThum <DrThum@users.noreply.github.com>
2010-11-03 03:27:22 +0100

    Fixed item 31331 proc effect displayed as debuff

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -568,6 +568,7 @@ bool IsPositiveEffect(uint32 spellId, uint32 effIndex)
         case 31579:                                         // Arcane Empowerment Rank1 talent aura with one positive and one negative (check not needed in wotlk)
         case 31582:                                         // Arcane Empowerment Rank2
         case 31583:                                         // Arcane Empowerment Rank3
+        case 38307:
             return true;
         case  1852:                                         // Silenced (GM)
         case 46392:                                         // Focused Assault
