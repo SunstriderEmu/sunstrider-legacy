83720dd0ecaf5b2a1ea3490b1a00b2ecce48f1df
DrThum <DrThum@users.noreply.github.com>
2010-11-11 01:20:48 +0100

    Fixed equipmentCache

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -15970,7 +15970,7 @@ void Player::SaveToDB()
     for (uint32 i = 0; i < 128; ++i)
         ss << GetUInt32Value(128 + i) << " ";
     ss << "', '";
-    for (uint32 i = 0; i < EQUIPMENT_SLOT_END * 2; ++i )
+    for (uint32 i = 0; i < 304; ++i)
         ss << GetUInt32Value(PLAYER_VISIBLE_ITEM_1_CREATOR + i) << " ";
     ss << "', ";
     ss << GetUInt32Value(PLAYER_AMMO_ID) << ", '";
