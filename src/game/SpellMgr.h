a0c329feb4e6fb7be6f8939dda06f1c7d3e963fb
DrThum <DrThum@users.noreply.github.com>
2010-12-08 22:57:59 +0100

    Fixed Shattrath flasks stack bug

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -800,7 +800,9 @@ class SpellMgr
         SpellSpecific GetSpellElixirSpecific(uint32 spellid) const
         {
             uint32 mask = GetSpellElixirMask(spellid);
-            if((mask & ELIXIR_FLASK_MASK)==ELIXIR_FLASK_MASK)
+            if (mask & ELIXIR_SHATTRATH_MASK)
+                return SPELL_FLASK_ELIXIR;
+            else if((mask & ELIXIR_FLASK_MASK)==ELIXIR_FLASK_MASK)
                 return SPELL_FLASK_ELIXIR;
             else if(mask & ELIXIR_BATTLE_MASK)
                 return SPELL_BATTLE_ELIXIR;
