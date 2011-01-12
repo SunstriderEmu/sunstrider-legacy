62ffc63d3f0914913afe43dcb1b82b2bedf9a1af
DrThum <DrThum@users.noreply.github.com>
2011-01-12 18:16:34 +0100

    Re-allow to cast Curse of Doom on pets, as it is now dispellable

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5238,8 +5238,8 @@ bool Spell::CheckTargetCreatureType(Unit* target) const
     // Curse of Doom : not find another way to fix spell target check :/
     if(m_spellInfo->SpellFamilyName==SPELLFAMILY_WARLOCK && m_spellInfo->SpellFamilyFlags == 0x0200000000LL)
     {
-        // not allow cast at player and not at pet (bug abuse, must be removed when pet's buffs will be handled correctly)
-        if(target->GetTypeId()==TYPEID_PLAYER || target->ToCreature()->isPet())
+        // not allow cast at player
+        if(target->GetTypeId()==TYPEID_PLAYER)
             return false;
 
         spellCreatureTargetMask = 0x7FF;
