623925024458dfa787fe705f2078486827684d85
DrThum <DrThum@users.noreply.github.com>
2011-07-30 13:17:14 +0200

    Fixed quests 10040 and 10041

---
+++ build/sql/updates/3418_world.sql
@@ -0,0 +1,4 @@
+INSERT INTO spell_linked_spell VALUES (-32756, -38081, 0, "Quest 10040 and 40041");
+INSERT INTO spell_linked_spell VALUES (-32756, -38080, 0, "Quest 10040 and 40041");
+
+UPDATE creature_template SET scriptname = "npc_scout_neftis" WHERE entry = 18714;
