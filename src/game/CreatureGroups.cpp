df01a0c816e7552fc72a61e402d119f3a4b3e994
DrThum <DrThum@users.noreply.github.com>
2011-08-08 14:55:17 +0200

    Last one I swear

--- src/game/CreatureGroups.cpp
+++ src/game/CreatureGroups.cpp
@@ -288,6 +288,7 @@ void CreatureGroup::CheckLeaderDistance(Creature* member)
         
     if (member->GetDistance(m_leader) < dist_min) {
         member->AddUnitMovementFlag(MOVEMENTFLAG_WALK_MODE);
+        return;
         //member->GetMotionMaster()->MovePoint(0, m_leaderX, m_leaderY, m_leaderZ);
     }
     else if (member->GetDistance(m_leader) > dist_max) {
