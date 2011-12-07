78fd9ce98a5df7cdf309888b0042b61724ee549f
DrThum <DrThum@users.noreply.github.com>
2011-12-07 16:57:46 +0100

    Tiny cleanup

--- src/game/SmartScript.cpp
+++ src/game/SmartScript.cpp
@@ -117,11 +117,9 @@ void SmartScript::ProcessAction(SmartScriptHolder &e, Unit* unit, uint32 var0, u
     {
         SmartScriptHolder linked = FindLinkedEvent(e.link);
         if (linked.GetActionType() && linked.GetEventType() == SMART_EVENT_LINK)
-        {
             ProcessEvent(linked, unit, var0, var1, bvar, spell, gob);
-        }else{
+        else
             sLog.outErrorDb("SmartScript::ProcessAction: Entry %d SourceType %u, Event %u, Link Event %u not found or invalid, skipped.", e.entryOrGuid, e.GetScriptType(), e.event_id, e.link);
-        }
     }
 
     switch (e.GetActionType())
