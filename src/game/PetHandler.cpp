a314c9b11f2807f37c2a1308cf9ed63b1960a908
DrThum <DrThum@users.noreply.github.com>
2012-01-01 20:37:37 +0100

    Mage: Fixed distance check for water elemental Freeze spell

--- src/game/PetHandler.cpp
+++ src/game/PetHandler.cpp
@@ -214,7 +214,6 @@ void WorldSession::HandlePetAction( WorldPacket & recv_data )
             pet->clearUnitState(UNIT_STAT_FOLLOW);
 
             Spell *spell = new Spell(pet, spellInfo, false);
-
             int16 result = spell->PetCanCast(unit_target);
 
                                                             //auto turn to target unless possessed
@@ -662,7 +661,7 @@ void WorldSession::HandlePetCastSpellOpcode( WorldPacket& recvPacket )
 
     caster->clearUnitState(UNIT_STAT_FOLLOW);
 
-    Spell *spell = new Spell(caster, spellInfo, spellid == 33395); // water elemental can cast freeze as triggered
+    Spell *spell = new Spell(caster, spellInfo, false);
     spell->m_targets = targets;
 
     int16 result = spell->PetCanCast(NULL);
