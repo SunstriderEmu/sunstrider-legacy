b9978b1717f78728e124cfa808f906ff7d1ecbf6
DrThum <DrThum@users.noreply.github.com>
2012-02-20 00:05:35 +0100

    Hunter's pet now follow when switched to passive mode

--- src/game/PetHandler.cpp
+++ src/game/PetHandler.cpp
@@ -173,6 +173,11 @@ void WorldSession::HandlePetAction( WorldPacket & recv_data )
             switch(spellid)
             {
                 case REACT_PASSIVE:                         //passive
+                    pet->AttackStop();
+                    pet->InterruptNonMeleeSpells(false);
+                    pet->GetMotionMaster()->MoveFollow(_player, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
+                    charmInfo->SetCommandState(COMMAND_FOLLOW);
+                    // No break
                 case REACT_DEFENSIVE:                       //recovery
                 case REACT_AGGRESSIVE:                      //activete
                     if(pet->GetTypeId() == TYPEID_UNIT)
