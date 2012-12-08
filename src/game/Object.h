06956f1ddf035feae6b0e5768e9315c73e7eacec
mathman <mathman.pro@gmail.com>
2012-12-08 20:47:32 +0100

    Core/Grid : Add FindNearestPlayer Script/KilJaeden : Attempt to fix link and aggro

--- src/game/Object.h
+++ src/game/Object.h
@@ -617,6 +617,7 @@ class WorldObject : public Object, public WorldLocation
         Creature* SummonTrigger(float x, float y, float z, float ang, uint32 dur, CreatureAI* (*GetAI)(Creature*) = NULL);
         Creature*   FindNearestCreature(uint32 entry, float range, bool alive = true);
         GameObject* FindNearestGameObject(uint32 entry, float range);
+        Player* FindNearestPlayer(float range);
         bool isActiveObject() const { return m_isActive; }
         void setActive(bool isActiveObject);
         void SetWorldObject(bool apply);
