d574721c88a3f6e1977b31711247452b6dffaa44
Root <root@captain.wowmania.fr>
2012-12-19 03:59:38 +0100

    Change some settings in the compilation script because of the new server

--- contrib/wowmania/clean_and_rebuild.sh
+++ contrib/wowmania/clean_and_rebuild.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 
-TC_DIR="/opt/trinitycore"
+TC_DIR="/home/trinitycore"
 
 [[ -n "$1" ]] && MAKEOPTS="-j$1" || MAKEOPTS="-j1"
 
 cd "${TC_DIR}/build"
 
-#rm -rf * 
-
 echo 1 > ${TC_DIR}/config/maintenance.conf
 
 #FLAGS="-O3 -march=nocona -ggdb3 -pipe -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-strength-reduce -fno-delete-null-pointer-checks -fno-strict-aliasing -msse2 -ftree-vectorize -fomit-frame-pointer -funroll-loops -m3dnow"
 #FLAGS="-O2 -march=nocona -pipe -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-strength-reduce -fno-delete-null-pointer-checks -fno-strict-aliasing -msse2 -m3dnow -fno-inline"
-FLAGS="-O2 -march=nocona -pipe -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-delete-null-pointer-checks -fno-strict-aliasing -frename-registers -fno-omit-frame-pointer -ggdb -minline-all-stringops -msse3"
+FLAGS="-O2 -march=native -pipe -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-delete-null-pointer-checks -fno-strict-aliasing -frename-registers -fno-omit-frame-pointer -ggdb -minline-all-stringops -msse3"
 export CFLAGS="${FLAGS}"
 export CXXFLAGS="${FLAGS}"
 
 cmake \
     -DPREFIX="${TC_DIR}/" \
     -DCONF_DIR="${TC_DIR}/config" \
-    -DCMAKE_C_COMPILER="/usr/bin/gcc-4.3.4" \
-    -DCMAKE_CXX_COMPILER="/usr/bin/g++-4.3.4" \
+    -DCMAKE_C_COMPILER="/usr/bin/gcc-4.3.6" \
+    -DCMAKE_CXX_COMPILER="/usr/bin/g++-4.3.6" \
     -DCMAKE_C_FLAGS="${CFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DACE_LIBRARY="/usr/lib/libACE.so" \
