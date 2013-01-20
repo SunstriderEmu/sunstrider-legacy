43699260d89d2702990149a2ddf796d50a327827
DrThum <DrThum@users.noreply.github.com>
2013-01-20 20:36:40 +0100

    Teleport to homebind when leaving a group in instance in all cases

--- src/game/Group.cpp
+++ src/game/Group.cpp
@@ -1665,10 +1665,9 @@ void Group::_homebindIfInstance(Player *player)
     if(player && !player->isGameMaster() && sMapStore.LookupEntry(player->GetMapId())->IsDungeon())
     {
         // leaving the group in an instance, the homebind timer is started
-        // unless the player is permanently saved to the instance
-        InstanceSave *save = sInstanceSaveManager.GetInstanceSave(player->GetInstanceId());
+        /*InstanceSave *save = sInstanceSaveManager.GetInstanceSave(player->GetInstanceId());
         InstancePlayerBind *playerBind = save ? player->GetBoundInstance(save->GetMapId(), save->GetDifficulty()) : NULL;
-        if(!playerBind || !playerBind->perm)
+        if(!playerBind || !playerBind->perm)*/
             player->m_InstanceValid = false;
     }
 }
