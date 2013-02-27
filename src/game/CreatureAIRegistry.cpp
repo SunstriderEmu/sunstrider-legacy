9e87a13a5501d5ee34c9cfe6e192461652097f92
DrThum <DrThum@users.noreply.github.com>
2013-02-27 20:16:30 +0100

    Added back AOEAI, let's see if it fixes Illidan bugs

--- src/game/CreatureAIRegistry.cpp
+++ src/game/CreatureAIRegistry.cpp
@@ -22,6 +22,7 @@
 #include "NullCreatureAI.h"
 #include "ReactorAI.h"
 #include "AggressorAI.h"
+#include "AOEAI.h"
 #include "ArcherAI.h"
 #include "GuardAI.h"
 #include "PetAI.h"
@@ -51,6 +52,7 @@ namespace AIRegistry
         (new CreatureAIFactory<TotemAI>("TotemAI"))->RegisterSelf();
         (new CreatureAIFactory<OutdoorPvPObjectiveAI>("OutdoorPvPObjectiveAI"))->RegisterSelf();
         (new CreatureAIFactory<PossessedAI>("PossessedAI"))->RegisterSelf();
+        (new CreatureAIFactory<AOEAI>("AOEAI"))->RegisterSelf();
         (new CreatureAIFactory<ArcherAI>("ArcherAI"))->RegisterSelf();
         (new CreatureAIFactory<SmartAI>("SmartAI"))->RegisterSelf();
         
