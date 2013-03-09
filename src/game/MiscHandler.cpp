56d28e6cd880e7c9ac5e6053592be2e6b596db49
DrThum <DrThum@users.noreply.github.com>
2013-03-09 20:36:05 +0100

    Don't update fleeing movement if the target is falling

--- src/game/MiscHandler.cpp
+++ src/game/MiscHandler.cpp
@@ -382,7 +382,7 @@ void WorldSession::HandleLogoutRequestOpcode( WorldPacket & /*recv_data*/ )
         return;
     }
 
-    //instant logout in taverns/cities or on taxi or for admins, gm's, mod's if its enabled in mangosd.conf
+    //instant logout in taverns/cities or on taxi or for admins, gm's, mod's if its enabled in config
     if (GetPlayer()->HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_RESTING) || GetPlayer()->isInFlight() ||
         GetSecurity() >= sWorld.getConfig(CONFIG_INSTANT_LOGOUT))
     {
