3f520ebf29bf5b99bf6f8d87d02607537721cdc3
DrThum <DrThum@users.noreply.github.com>
2013-06-09 17:08:51 +0200

    Fixed quest 11169

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12066,6 +12066,8 @@ void Unit::Kill(Unit *pVictim, bool durabilityLoss)
                 if (Creature* totem = Unit::GetCreature(*this, m_TotemSlot[slot]))
                     totem->AI()->MasterKilledUnit(pVictim);
             }
+            if (Creature* totem = Unit::GetCreature(*this, m_TotemSlot254)) // Slot for some quest totems
+                totem->AI()->MasterKilledUnit(pVictim);
         }
 
         // last damage from non duel opponent or opponent controlled creature
@@ -12115,6 +12117,12 @@ void Unit::Kill(Unit *pVictim, bool durabilityLoss)
                 if (pet->IsAIEnabled)
                     pet->AI()->MasterKilledUnit(pVictim);
             }
+            for (uint8 slot = 0; slot < MAX_TOTEM; slot++) {
+                if (Creature* totem = Unit::GetCreature(*this, m_TotemSlot[slot]))
+                    totem->AI()->MasterKilledUnit(pVictim);
+            }
+            if (Creature* totem = Unit::GetCreature(*this, m_TotemSlot254)) // Slot for some quest totems
+                totem->AI()->MasterKilledUnit(pVictim);
         }
 
         // Call creature just died function
