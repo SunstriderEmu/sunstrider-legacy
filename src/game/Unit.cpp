9076323dd6005e0c84b2cc8ca2465c3ead6aff4a
DrThum <DrThum@users.noreply.github.com>
2013-06-09 14:00:26 +0200

    Also relay CreatureAI::MasterKilledUnit call to totems

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12062,6 +12062,10 @@ void Unit::Kill(Unit *pVictim, bool durabilityLoss)
                 if (pet->IsAIEnabled)
                     pet->AI()->MasterKilledUnit(pVictim);
             }
+            for (uint8 slot = 0; slot < MAX_TOTEM; slot++) {
+                if (Creature* totem = Unit::GetCreature(*this, m_TotemSlot[slot]))
+                    totem->AI()->MasterKilledUnit(pVictim);
+            }
         }
 
         // last damage from non duel opponent or opponent controlled creature
