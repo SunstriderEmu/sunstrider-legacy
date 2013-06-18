a4a16b3211ac4caf7f0d55b3e5955218356189a1
Kelno <kelnowm@gmail.com>
2013-06-18 11:03:29 +0200

    CONFIG_WHISPER_MINLEVEL wont apply to addon language anymore (this could cause some severe spam in some cases)

--- src/game/ChatHandler.cpp
+++ src/game/ChatHandler.cpp
@@ -255,7 +255,7 @@ void WorldSession::HandleMessagechatOpcode( WorldPacket & recv_data )
             }
 
             //can still whisper GM's
-            if (pSecurity <= SEC_PLAYER && GetPlayer()->GetSession()->GetSecurity() <= SEC_PLAYER && GetPlayer()->getLevel() < sWorld.getConfig(CONFIG_WHISPER_MINLEVEL)) {
+            if (pSecurity <= SEC_PLAYER && GetPlayer()->GetSession()->GetSecurity() <= SEC_PLAYER && GetPlayer()->getLevel() < sWorld.getConfig(CONFIG_WHISPER_MINLEVEL) && lang != LANG_ADDON) {
                 ChatHandler(this).PSendSysMessage("Votre niveau est insuffisant pour chuchoter aux autres joueurs (niveau %u requis).", sWorld.getConfig(CONFIG_WHISPER_MINLEVEL));
                 break;
             }
