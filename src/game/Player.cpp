f07a0db39b4040d5eb317804bd52c2c25e4a5e78
Kelno <kelnowm@gmail.com>
2013-06-20 18:31:20 +0200

    Moar lava, enabled in blackrock mountain (any other places to add?)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -20179,11 +20179,11 @@ void Player::UpdateUnderwaterState(Map* m, float x, float y, float z)
     if (!res)
     {
         m_MirrorTimerFlags &= ~(UNDERWATER_INWATER|UNDERWATER_INLAVA|UNDERWATER_INSLIME|UNDERWATER_INDARKWATER);
-        // Small hack for enable breath in WMO + enable lava in Molten Core and BlackRock Dephts
+        // Small hack for enable breath in WMO + enable lava in Molten Core, Blackrock Mountain and BlackRock Dephts
         if (IsInWater())
         {
             m_MirrorTimerFlags|=UNDERWATER_INWATER;
-            if(GetMapId() == 409 || GetMapId() == 230)
+            if(GetMapId() == 409 || GetZoneId() = 25 || GetMapId() == 230)
                 m_MirrorTimerFlags |= UNDERWATER_INLAVA;
         }
         return;
