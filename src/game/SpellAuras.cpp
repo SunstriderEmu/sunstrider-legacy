d2dc18b24a4a9440cb760fb6cd6fda19143cc76e
Kelno <kelnowm@gmail.com>
2013-06-24 12:35:28 +0200

    Wild guess fix to the shadowform bug

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2873,11 +2873,6 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
             m_target->RemoveAurasDueToSpell(m_target->m_ShapeShiftFormSpellId,this);
 
         m_target->SetByteValue(UNIT_FIELD_BYTES_2, 3, form);
-        
-        // force update as too quick shapeshifting and back
-        // causes the value to stay the same serverside
-        // causes issues clientside (player gets stuck)
-        m_target->ForceValuesUpdateAtIndex(UNIT_FIELD_BYTES_2);
 
         if(modelid > 0)
             m_target->SetDisplayId(modelid);
@@ -2987,6 +2982,11 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
 
     if(m_target->GetTypeId()==TYPEID_PLAYER)
         (m_target->ToPlayer())->InitDataForForm();
+
+    // force update as too quick shapeshifting and back
+    // causes the value to stay the same serverside
+    // causes issues clientside (player gets stuck)
+    m_target->ForceValuesUpdateAtIndex(UNIT_FIELD_BYTES_2);
 }
 
 void Aura::HandleAuraTransform(bool apply, bool Real)
