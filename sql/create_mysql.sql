2cf0114eb2aac3a3a108420a110c8d85f93923a3
Kelno <kelnowm@gmail.com>
2013-08-10 16:12:48 +0200

    Updated create_mysql.sql

--- sql/create_mysql.sql
+++ sql/create_mysql.sql
@@ -6,9 +6,12 @@ CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE DATABASE `realmd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+CREATE DATABASE `logs` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 GRANT ALL PRIVILEGES ON `world` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
 
 GRANT ALL PRIVILEGES ON `characters` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
 
 GRANT ALL PRIVILEGES ON `realmd` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
 
+GRANT ALL PRIVILEGES ON `logs` . * TO 'trinity'@'localhost' WITH GRANT OPTION;
