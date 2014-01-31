7ebe93d43c44834889743c6f0bca2128e9de26a8
Kelno <kelnowm@gmail.com>
2014-01-31 22:07:57 +0100

    Minor updates on Kil'Jaeden

--- src/game/DynamicObject.cpp
+++ src/game/DynamicObject.cpp
@@ -87,10 +87,14 @@ bool DynamicObject::Create( uint32 guidlow, Unit *caster, uint32 spellId, uint32
     float visualRadius = radius;
     // For some reason visual size in client seems incorrect for some spells. Can't seem to find the proper rule.
     if(SpellEntry const *spellInfo = spellmgr.LookupSpell(spellId))
+    {
         if(    spellInfo->rangeIndex == 1      //personal range. Ice trap, consecration,...
             || spellInfo->SpellVisual == 10383 //flamestrike
-            ) 
+          ) 
             visualRadius = radius*2.0; 
+        else if (spellInfo->Id == 45848)
+            visualRadius = radius*0.5;
+    }
 
     SetEntry(spellId);
     SetFloatValue( OBJECT_FIELD_SCALE_X, 1 );
