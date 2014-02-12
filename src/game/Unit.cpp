3720bc2fc810434f58c4bb8ca6a6d581a9da36c9
Kelno <kelnowm@gmail.com>
2014-02-12 09:13:52 +0100

    Totems should not be immune to all spells with any dots, but just to auras in general

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8770,14 +8770,8 @@ bool Unit::IsImmunedToSpell(SpellEntry const* spellInfo, bool useCharges)
     }
 
     if(ToCreature() && ToCreature()->isTotem())
-    {
-        for(uint8 i = 0; i < MAX_SPELL_EFFECTS;i++)
-            if (spellInfo->EffectApplyAuraName[i] == SPELL_AURA_PERIODIC_DAMAGE)
-                return true;
-
         if(IsChanneledSpell(spellInfo))
             return true;
-    }
 
     return false;
 }
