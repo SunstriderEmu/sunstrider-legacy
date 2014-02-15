e8cd999cecf36a0dede912b5ed451e3caee65bf9
SCHMIDT Julien <julien.schmidt@wanadoo.fr>
2014-02-15 17:40:27 +0100

    [L-10] Purification de la viande de sanglier infernal (Quête : 10369).

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2962,6 +2962,9 @@ void SpellMgr::LoadSpellCustomAttr()
             spellInfo->AttributesEx3 |= SPELL_ATTR_EX3_NO_INITIAL_AGGRO;
             //spellInfo->AttributesEx |= SPELL_ATTR_EX_NOT_BREAK_STEALTH; // Check if it wasn't changed later (in 3.x)
             break;
+        case 29200: // Purification de la viande de sanglier infernal
+            spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
+            break;
         case 20625:
             spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_PARTY_CASTER;
             break;
