f1a93d831e6fa577aa07cd5b93bc5948b82aca44
SCHMIDT Julien <julien.schmidt@wanadoo.fr>
2014-02-22 00:42:01 +0100

    [L-14] FIX : GAMEOBJECT_TYPE_FISHINGNODE.

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -285,11 +285,8 @@ void GameObject::Update(uint32 diff)
                             Unit* caster = GetOwner();
                             if(caster && caster->GetTypeId()==TYPEID_PLAYER)
                             {
-                                if(caster->m_currentSpells[CURRENT_CHANNELED_SPELL])
-                                {
-                                    caster->m_currentSpells[CURRENT_CHANNELED_SPELL]->SendChannelUpdate(0);
-                                    caster->m_currentSpells[CURRENT_CHANNELED_SPELL]->finish(false);
-                                }
+                                caster->m_currentSpells[CURRENT_CHANNELED_SPELL]->SendChannelUpdate(0);
+                                caster->m_currentSpells[CURRENT_CHANNELED_SPELL]->finish(false);
 
                                 WorldPacket data(SMSG_FISH_NOT_HOOKED,0);
                                 (caster->ToPlayer())->GetSession()->SendPacket(&data);
