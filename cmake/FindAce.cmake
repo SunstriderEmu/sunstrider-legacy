305045796103b7b8643e1f0e45ec828d6c6ede13
Kelno <kelnowm@gmail.com>
2014-06-07 19:29:45 +0200

    First cmake changes

--- cmake/FindAce.cmake
+++ cmake/FindAce.cmake
@@ -17,9 +17,6 @@ MACRO(FIND_ACE LIBNAME)
         DOC "Path to ace/ACE.h"
     )
 
-    # This prevents it being taken from cache. - but also broke cmake -i, so we dont use it
-    # SET(ACE_LIBRARY ACE_LIBRARY-NOTFOUND)
-
     FIND_LIBRARY(
         ACE_LIBRARY "${LIBNAME}"
         PATHS /usr/lib /usr/local/lib
