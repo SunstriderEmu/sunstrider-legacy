4306192d5fcda2209cfbc0220ec436c7a18d9f64
Kelno <kelnowm@gmail.com>
2015-04-12 20:59:45 +0200

    (OregonCore) Fixed loot owner for spell Extract Gas (30427)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1575,7 +1575,7 @@ void Aura::TriggerSpell()
                             }
                             Loot *loot = &creature->loot;
                             loot->clear();
-                            loot->FillLoot(creature->GetCreatureTemplate()->SkinLootId, LootTemplates_Skinning, NULL);
+                            loot->FillLoot(creature->GetCreatureTemplate()->SkinLootId, LootTemplates_Skinning, player);
                             for(uint8 i=0;i<loot->items.size();i++)
                             {
                                 LootItem *item = loot->LootItemInSlot(i,player);
