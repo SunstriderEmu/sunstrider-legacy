8a1023f43dbb9a8f850a82be8ee0e707c755cf0c
Kelno <kelnowm@gmail.com>
2015-05-07 14:15:12 +0200

    TrinityCore/TrinityCore@c54cbebf8863e41c240e6ce5f8ce849fda122d38

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12340,11 +12340,21 @@ void Unit::SetControlled(bool apply, UnitState state)
         {
             case UNIT_STATE_STUNNED:
                 SetStunned(true);
+                // We need to stop fear on stun and root or we will get teleport to destination issue as MVMGEN for fear keeps going on		
+                if (HasUnitState(UNIT_STATE_FLEEING))
+                    SetFeared(false);
+
                 CastStop();
                 break;
             case UNIT_STATE_ROOT:
                 if (!HasUnitState(UNIT_STATE_STUNNED))
+                {
+                    // We need to stop fear on stun and root or we will get teleport to destination issue as MVMGEN for fear keeps going on		
+                    if (HasUnitState(UNIT_STATE_FLEEING))
+                        SetFeared(false);
+
                     SetRooted(true);
+                }
                 break;
             case UNIT_STATE_CONFUSED:
                 if (!HasUnitState(UNIT_STATE_STUNNED))
@@ -12431,11 +12441,9 @@ void Unit::SetStunned(bool apply)
         // setting MOVEMENTFLAG_ROOT
         RemoveUnitMovementFlag(MOVEMENTFLAG_MASK_MOVING);
         AddUnitMovementFlag(MOVEMENTFLAG_ROOT);
+        StopMoving();
 
-        // Creature specific
-        if (GetTypeId() != TYPEID_PLAYER)
-            StopMoving();
-        else
+        if (GetTypeId() == TYPEID_PLAYER)
             SetStandState(UNIT_STAND_STATE_STAND);
 
         WorldPacket data(SMSG_FORCE_MOVE_ROOT, 8);
@@ -12481,6 +12489,7 @@ void Unit::SetRooted(bool apply)
         // setting MOVEMENTFLAG_ROOT
         RemoveUnitMovementFlag(MOVEMENTFLAG_MASK_MOVING);
         AddUnitMovementFlag(MOVEMENTFLAG_ROOT);
+        StopMoving();
 
         if (GetTypeId() == TYPEID_PLAYER)
         {
@@ -12494,7 +12503,6 @@ void Unit::SetRooted(bool apply)
             WorldPacket data(SMSG_SPLINE_MOVE_ROOT, 8);
             data.append(GetPackGUID());
             SendMessageToSet(&data, true);
-            StopMoving();
         }
     }
     else
