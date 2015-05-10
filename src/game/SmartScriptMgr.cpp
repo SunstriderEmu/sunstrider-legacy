45084be56b6553cdd249b397ebf8c308cc2216e7
Kelno <kelnowm@gmail.com>
2015-05-10 19:54:41 +0200

    Fixed https://github.com/SunstriderProject/Sunstrider-Issues/issues/86
    Former-commit-id: e6abbe638bd2f8ffc42ad3bbbb6019ab507f6546

--- src/game/SmartScriptMgr.cpp
+++ src/game/SmartScriptMgr.cpp
@@ -1133,6 +1133,7 @@ bool SmartAIMgr::IsEventValid(SmartScriptHolder& e)
         case SMART_ACTION_SUMMON_CREATURE_GROUP:
         case SMART_ACTION_LOAD_PATH:
         case SMART_ACTION_TELEPORT_ON_ME:
+        case SMART_ACTION_SELF_TELEPORT_ON_TARGET:
             break;
         default:
             SMARTAI_DB_ERROR( e.entryOrGuid, "SmartAIMgr: Not handled action_type(%u), event_type(%u), Entry %d SourceType %u Event %u, skipped.", e.GetActionType(), e.GetEventType(), e.entryOrGuid, e.GetScriptType(), e.event_id);
