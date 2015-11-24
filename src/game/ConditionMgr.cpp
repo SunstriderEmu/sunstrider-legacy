b9548efd6a22b0136406d6f285148794a6471a57
Kelno <kelnowm@gmail.com>
2015-11-24 14:57:33 +0100

    [TrinityCore] Core/Condition: Fix issue reported by static analysis
    Former-commit-id: 858bf22b60d3995e032572ecb8f51c7389a09d03

--- src/game/ConditionMgr.cpp
+++ src/game/ConditionMgr.cpp
@@ -328,7 +328,7 @@ bool Condition::Meets(ConditionSourceInfo& sourceInfo)
                 Unit* unit = object->ToUnit();
                 if (toUnit && unit)
                 {
-                    switch (ConditionValue2)
+                    switch (static_cast<RelationType>(ConditionValue2))
                     {
                         case RELATION_SELF:
                             condMeets = unit == toUnit;
