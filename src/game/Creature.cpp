2480f24a8114791aefe005264def551d190112cb
Kelno <kelnowm@gmail.com>
2016-03-05 22:58:39 +0100

    Fixed logic in Creature::UpdateMovementFlags that was making server spamming packets in some cases
    Former-commit-id: 2d64f9b5e48b3295eedb41da129ce1a5ee510dee

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -2651,6 +2651,7 @@ void Creature::UpdateMovementFlags()
 
     bool isInAir = (G3D::fuzzyGt(GetPositionZMinusOffset(), ground + 0.05f) || G3D::fuzzyLt(GetPositionZMinusOffset(), ground - 0.05f)); // Can be underground too, prevent the falling
 
+    bool canFly = CanFly();
     if (CanFly() && isInAir && !IsFalling())
     {
         if (CanWalk())
@@ -2667,7 +2668,7 @@ void Creature::UpdateMovementFlags()
         SetDisableGravity(false);
     }
 
-    if (!isInAir)
+    if (!isInAir || canFly)
         RemoveUnitMovementFlag(MOVEMENTFLAG_JUMPING_OR_FALLING);
 
     SetSwim(CanSwim() && IsInWater());
