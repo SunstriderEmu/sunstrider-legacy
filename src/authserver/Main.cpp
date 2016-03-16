921e2b0ebebaf093d3b9a3135ae4a32f7a2ab3d6
Kelno <kelnowm@gmail.com>
2016-03-16 01:03:46 +0100

    Updated authserver from TC
    Former-commit-id: d7439256ba7619a0de6bcc30ac7656f0919ec0eb

--- src/authserver/Main.cpp
+++ src/authserver/Main.cpp
@@ -48,19 +48,23 @@ bool StartDB();
 void StopDB();
 void SignalHandler(const boost::system::error_code& error, int signalNumber);
 void KeepDatabaseAliveHandler(const boost::system::error_code& error);
-variables_map GetConsoleArguments(int argc, char** argv, std::string& configFile);
+void BanExpiryHandler(boost::system::error_code const& error);
+variables_map GetConsoleArguments(int argc, char** argv, std::string& configFile, std::string& configService);
 
-boost::asio::io_service _ioService;
-boost::asio::deadline_timer _dbPingTimer(_ioService);
+boost::asio::io_service* _ioService;
+boost::asio::deadline_timer* _dbPingTimer;
 uint32 _dbPingInterval;
+boost::asio::deadline_timer* _banExpiryCheckTimer;
+uint32 _banExpiryCheckInterval;
 LoginDatabaseWorkerPool LoginDatabase;
 
 int main(int argc, char** argv)
 {
     std::string configFile = _TRINITY_REALM_CONFIG;
-    auto vm = GetConsoleArguments(argc, argv, configFile);
+    std::string configService;
+    auto vm = GetConsoleArguments(argc, argv, configFile, configService);
     // exit if help is enabled
-    if (vm.count("help"))
+    if (vm.count("help") || vm.count("version"))
         return 0;
 
     std::string configError;
@@ -93,13 +97,16 @@ int main(int argc, char** argv)
     if (!StartDB())
         return 1;
 
-    // Get the list of realms for the server
-    sRealmList->Initialize(_ioService, sConfigMgr->GetIntDefault("RealmsStateUpdateDelay", 20));
+    _ioService = new boost::asio::io_service();
 
-    if (sRealmList->size() == 0)
+    // Get the list of realms for the server
+    sRealmList->Initialize(*_ioService, sConfigMgr->GetIntDefault("RealmsStateUpdateDelay", 20));
+
+    if (sRealmList->GetRealms().empty())
     {
         TC_LOG_ERROR("server.authserver", "No valid realms specified.");
         StopDB();
+        delete _ioService;
         return 1;
     }
 
@@ -114,10 +121,10 @@ int main(int argc, char** argv)
 
     std::string bindIp = sConfigMgr->GetStringDefault("BindIP", "0.0.0.0");
 
-    sAuthSocketMgr.StartNetwork(_ioService, bindIp, port);
+    sAuthSocketMgr.StartNetwork(*_ioService, bindIp, port);
 
     // Set signal handlers
-    boost::asio::signal_set signals(_ioService, SIGINT, SIGTERM);
+    boost::asio::signal_set signals(*_ioService, SIGINT, SIGTERM);
 #if PLATFORM == PLATFORM_WINDOWS
     signals.add(SIGBREAK);
 #endif
@@ -128,16 +135,46 @@ int main(int argc, char** argv)
 
     // Enabled a timed callback for handling the database keep alive ping
     _dbPingInterval = sConfigMgr->GetIntDefault("MaxPingTime", 30);
-    _dbPingTimer.expires_from_now(boost::posix_time::minutes(_dbPingInterval));
-    _dbPingTimer.async_wait(KeepDatabaseAliveHandler);
+    _dbPingTimer = new boost::asio::deadline_timer(*_ioService);
+    _dbPingTimer->expires_from_now(boost::posix_time::minutes(_dbPingInterval));
+    _dbPingTimer->async_wait(KeepDatabaseAliveHandler);
+
+    _banExpiryCheckInterval = sConfigMgr->GetIntDefault("BanExpiryCheckInterval", 60);
+    _banExpiryCheckTimer = new boost::asio::deadline_timer(*_ioService);
+    _banExpiryCheckTimer->expires_from_now(boost::posix_time::seconds(_banExpiryCheckInterval));
+    _banExpiryCheckTimer->async_wait(BanExpiryHandler);
+
+#if PLATFORM == PLATFORM_WINDOWS
+    /* Whatever
+    if (m_ServiceStatus != -1)
+    {
+        _serviceStatusWatchTimer = new boost::asio::deadline_timer(*_ioService);
+        _serviceStatusWatchTimer->expires_from_now(boost::posix_time::seconds(1));
+        _serviceStatusWatchTimer->async_wait(ServiceStatusWatcher);
+    }
+    */
+#endif
 
     // Start the io service worker loop
-    _ioService.run();
+    _ioService->run();
+
+    _banExpiryCheckTimer->cancel();
+    _dbPingTimer->cancel();
+
+    sAuthSocketMgr.StopNetwork();
+
+    sRealmList->Close();
 
     // Close the Database Pool and library
     StopDB();
 
     TC_LOG_INFO("server.authserver", "Halting process...");
+
+    signals.cancel();
+
+    delete _banExpiryCheckTimer;
+    delete _dbPingTimer;
+    delete _ioService;
     return 0;
 }
 
@@ -190,7 +227,7 @@ void StopDB()
 void SignalHandler(const boost::system::error_code& error, int /*signalNumber*/)
 {
     if (!error)
-        _ioService.stop();
+        _ioService->stop();
 }
 
 void KeepDatabaseAliveHandler(const boost::system::error_code& error)
@@ -200,18 +237,40 @@ void KeepDatabaseAliveHandler(const boost::system::error_code& error)
         TC_LOG_INFO("server.authserver", "Ping MySQL to keep connection alive");
         LoginDatabase.KeepAlive();
 
-        _dbPingTimer.expires_from_now(boost::posix_time::minutes(_dbPingInterval));
-        _dbPingTimer.async_wait(KeepDatabaseAliveHandler);
+        _dbPingTimer->expires_from_now(boost::posix_time::minutes(_dbPingInterval));
+        _dbPingTimer->async_wait(KeepDatabaseAliveHandler);
     }
 }
 
-variables_map GetConsoleArguments(int argc, char** argv, std::string& configFile)
+void BanExpiryHandler(boost::system::error_code const& error)
+{
+    if (!error)
+    {
+        LoginDatabase.Execute(LoginDatabase.GetPreparedStatement(LOGIN_DEL_EXPIRED_IP_BANS));
+        LoginDatabase.Execute(LoginDatabase.GetPreparedStatement(LOGIN_UPD_EXPIRED_ACCOUNT_BANS));
+
+        _banExpiryCheckTimer->expires_from_now(boost::posix_time::seconds(_banExpiryCheckInterval));
+        _banExpiryCheckTimer->async_wait(BanExpiryHandler);
+    }
+}
+
+variables_map GetConsoleArguments(int argc, char** argv, std::string& configFile, std::string& configService)
 {
     options_description all("Allowed options");
     all.add_options()
         ("help,h", "print usage message")
         ("config,c", value<std::string>(&configFile)->default_value(_TRINITY_REALM_CONFIG), "use <arg> as configuration file")
         ;
+#if PLATFORM == PLATFORM_WINDOWS
+    options_description win("Windows platform specific options");
+    win.add_options()
+        ("service,s", value<std::string>(&configService)->default_value(""), "Windows service options: [install | uninstall]")
+        ;
+
+    all.add(win);
+#else
+    (void)configService;
+#endif
     variables_map variablesMap;
     try
     {
@@ -222,9 +281,10 @@ variables_map GetConsoleArguments(int argc, char** argv, std::string& configFile
         std::cerr << e.what() << "\n";
     }
 
-    if (variablesMap.count("help")) {
+    if (variablesMap.count("help"))
         std::cout << all << "\n";
-    }
+    else if (variablesMap.count("version"))
+        std::cout << _FULLVERSION << "\n";
 
     return variablesMap;
 }
