64b076b1bb33df5c18fc6bb5ad3f933fafc3f694
Kelno <kelnowm@gmail.com>
2016-04-30 20:48:14 +0200

    Fixed build
    Former-commit-id: 97619abeeaf4d7994f6f4990513d2b899459db61

--- src/playerbot/strategy/actions/BuyAction.cpp
+++ src/playerbot/strategy/actions/BuyAction.cpp
@@ -41,11 +41,14 @@ bool BuyAction::Execute(Event event)
     {
         for (uint32 slot = 0; slot < tItems->GetItemCount(); slot++)
         {
+            /* TODO PLAYERBOT
             if (tItems->GetItem(slot)->item == *i)
             {
                 bot->BuyItemFromVendorSlot(vendor->GetGUID(), slot, *i, 1, NULL_BAG, NULL_SLOT);
                 ai->TellMaster("Bought item");
             }
+            */
+            ai->TellMaster("NYI");
         }
     }
 
