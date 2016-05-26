1bf37ca0af7a62248bb6f4c588066e400d57f386
Kelno <kelnowm@gmail.com>
2016-05-26 16:24:13 +0200

    Added outland blades edge arena offmesh + Made mmaps_generator compatible with last maps version
    Former-commit-id: 2007e6b31facab5049bb9fbc75f1f764724841a0

--- src/tools/mmaps_generator/MapBuilder.cpp
+++ src/tools/mmaps_generator/MapBuilder.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include "MapBuilder.h"
 #include "PathCommon.h"
@@ -149,7 +132,7 @@ namespace MMAP
     {
         while (1)
         {
-            uint32 mapId;
+            uint32 mapId = 0;
 
             _queue.WaitAndPop(mapId);
 
@@ -198,7 +181,7 @@ namespace MMAP
         }
     }
     /**************************************************************************/
-    void MapBuilder::getGridBounds(uint32 mapID, uint32 &minX, uint32 &minY, uint32 &maxX, uint32 &maxY)
+    void MapBuilder::getGridBounds(uint32 mapID, uint32 &minX, uint32 &minY, uint32 &maxX, uint32 &maxY) const
     {
         // min and max are initialized to invalid values so the caller iterating the [min, max] range
         // will never enter the loop unless valid min/max values are found
@@ -684,7 +667,7 @@ namespace MMAP
         iv.polyMesh = rcAllocPolyMesh();
         if (!iv.polyMesh)
         {
-            printf("%s alloc iv.polyMesh FIALED!\n", tileString);
+            printf("%s alloc iv.polyMesh FAILED!\n", tileString);
             delete[] pmmerge;
             delete[] dmmerge;
             delete[] tiles;
@@ -695,7 +678,7 @@ namespace MMAP
         iv.polyMeshDetail = rcAllocPolyMeshDetail();
         if (!iv.polyMeshDetail)
         {
-            printf("%s alloc m_dmesh FIALED!\n", tileString);
+            printf("%s alloc m_dmesh FAILED!\n", tileString);
             delete[] pmmerge;
             delete[] dmmerge;
             delete[] tiles;
--- src/tools/mmaps_generator/TerrainBuilder.cpp
+++ src/tools/mmaps_generator/TerrainBuilder.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include "TerrainBuilder.h"
 
@@ -24,7 +7,6 @@
 #include "VMapManager2.h"
 #include "MapTree.h"
 #include "ModelInstance.h"
-#include <vector>
 
 // ******************************************
 // Map file format defines
@@ -82,7 +64,7 @@ struct map_liquidHeader
 namespace MMAP
 {
 
-    char const* MAP_VERSION_MAGIC = "v1.3";
+    char const* MAP_VERSION_MAGIC = "v1.8";
 
     TerrainBuilder::TerrainBuilder(bool skipLiquid) : m_skipLiquid (skipLiquid){ }
     TerrainBuilder::~TerrainBuilder() { }
@@ -709,7 +691,7 @@ namespace MMAP
                         vertsY = tilesY + 1;
                         uint8* flags = liquid->GetFlagsStorage();
                         float* data = liquid->GetHeightStorage();
-                        uint8 type;
+                        uint8 type = NAV_EMPTY;
 
                         // convert liquid type to NavTerrain
                         switch (liquid->GetType())
@@ -723,9 +705,6 @@ namespace MMAP
                             break;
                         case 3:
                             type = NAV_SLIME;
-                            break;
-                        default:
-                            type = NAV_EMPTY;
                             break;
                         }
 
