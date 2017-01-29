0b9c40d2ca345f9ddea588bcac77a51f64efdb62
Kelno <kelnowm@gmail.com>
2017-01-29 13:51:30 +0100

    Fixed secsToTimeString language
    Former-commit-id: 5aeef695767cf3d28d7fa75ab43a9c9deec59e87

--- src/common/Utilities/Util.cpp
+++ src/common/Utilities/Util.cpp
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
- *
- * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
 #include "Util.h"
 
 #include "utf8.h"
@@ -138,15 +118,15 @@ std::string secsToTimeString(uint32 timeInSecs, bool shortText, bool hoursOnly)
     //TODO translate
     std::ostringstream ss;
     if(days)
-        ss << days << (shortText ? "d" : " Jour(s) ");
+        ss << days << (shortText ? "d" : " Day(s) ");
     if(hours || hoursOnly)
-        ss << hours << (shortText ? "h" : " Heure(s) ");
+        ss << hours << (shortText ? "h" : " Hour(s) ");
     if(!hoursOnly)
     {
         if(minutes)
             ss << minutes << (shortText ? "m" : " Minute(s) ");
         if(secs || (!days && !hours && !minutes) )
-            ss << secs << (shortText ? "s" : " Seconde(s).");
+            ss << secs << (shortText ? "s" : " Second(s).");
     }
 
     return ss.str();
