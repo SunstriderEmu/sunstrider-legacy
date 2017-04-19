10d1b1db37f0181b693bea0e6ab504eb04c8d1c5
Kelno <kelnowm@gmail.com>
2017-04-19 13:43:31 +0200

    [TC] Core/Random: Remove no longer needed hack for VS 2013
    Former-commit-id: 4d0bad172c9ea3d1bed26227e1a6321913a11450

--- src/common/Utilities/Containers.h
+++ src/common/Utilities/Containers.h
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 #ifndef TRINITY_CONTAINERS_H
 #define TRINITY_CONTAINERS_H
@@ -83,8 +67,7 @@ namespace Trinity
         template <class C>
         typename C::const_iterator SelectRandomWeightedContainerElement(C const& container, std::vector<double> weights)
         {
-            Trinity::discrete_distribution_param<uint32> ddParam(weights.begin(), weights.end());
-            std::discrete_distribution<uint32> dd(ddParam);
+			std::discrete_distribution<uint32> dd(weights.begin(), weights.end());
             typename C::const_iterator it = container.begin();
             std::advance(it, dd(SFMTEngine::Instance()));
             return it;
