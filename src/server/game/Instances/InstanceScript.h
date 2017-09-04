3e87571caef9ab96acd9f73e6e4a53043ff636c3
Kelno <kelnowm@gmail.com>
2017-09-04 21:34:31 +0200

    Removed DialogHelper class. This breaks dialogs on Kil Jaeden fight, this will have to be re implemented another way.
    Former-commit-id: 44cfaab621d9c788ee382896a3b959b26e14d341

--- src/server/game/Instances/InstanceScript.h
+++ src/server/game/Instances/InstanceScript.h
@@ -1,6 +1,3 @@
-/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
-* This program is free software licensed under GPL version 2
-* Please see the included DOCS/LICENSE.TXT for more information */
 
 #ifndef SC_INSTANCE_H
 #define SC_INSTANCE_H
@@ -103,9 +100,6 @@ class TC_GAME_API InstanceScript : public ZoneScript
 
 		Map* instance;
 			
-        // Default accessor functions
-        Creature* GetSingleCreatureFromStorage(uint32 uiEntry, bool bSkipDebugLog = false);
-
         //All-purpose data storage 64 bit
         virtual uint64 GetData64(uint32 Data) const { return 0; }
         virtual void SetData64(uint32 Data, uint64 Value) { }
@@ -172,73 +166,10 @@ class TC_GAME_API InstanceScript : public ZoneScript
         DoorInfoMap doors;
         //NYI MinionInfoMap minions;
 
-        // Storage for GO-Guids and NPC-Guids
-        typedef std::map<uint32, uint64> EntryGuidMap;
-        EntryGuidMap m_mNpcEntryGuidStore;                  ///< Store unique NPC-Guids by entry
-
 #ifdef TRINITY_API_USE_DYNAMIC_LINKING
-                                                            // Strong reference to the associated script module
+        // Strong reference to the associated script module
         std::shared_ptr<ModuleReference> module_reference;
 #endif // #ifndef TRINITY_API_USE_DYNAMIC_LINKING
-};
-
-/// A static const array of this structure must be handled to DialogueHelper
-struct DialogueEntry
-{
-    int32 iTextEntry;                                       ///< To be said text entry
-    uint32 uiSayerEntry;                                    ///< Entry of the mob who should say
-    uint32 uiTimer;                                         ///< Time delay until next text of array is said (0 stops)
-};
-
-/// A static const array of this structure must be handled to DialogueHelper
-struct DialogueEntryTwoSide
-{
-    int32 iTextEntry;                                       ///< To be said text entry (first side)
-    uint32 uiSayerEntry;                                    ///< Entry of the mob who should say (first side)
-    int32 iTextEntryAlt;                                    ///< To be said text entry (second side)
-    uint32 uiSayerEntryAlt;                                 ///< Entry of the mob who should say (second side)
-    uint32 uiTimer;                                         ///< Time delay until next text of array is said (0 stops)
-};
-
-/// Helper class handling a dialogue given as static const array of DialogueEntry or DialogueEntryTwoSide
-class TC_GAME_API DialogueHelper
-{
-    public:
-        // The array MUST be terminated by {0,0,0}
-        DialogueHelper(DialogueEntry const* pDialogueArray);
-        // The array MUST be terminated by {0,0,0,0,0}
-        DialogueHelper(DialogueEntryTwoSide const* aDialogueTwoSide);
-
-        /// Function to initialize the dialogue helper for instances. If not used with instances, GetSpeakerByEntry MUST be overwritten to obtain the speakers
-        void InitializeDialogueHelper(InstanceScript* pInstance) { m_pInstance = pInstance; }
-        /// Set if take first entries or second entries
-        void SetDialogueSide(bool bIsFirstSide) { m_bIsFirstSide = bIsFirstSide; }
-
-        void StartNextDialogueText(int32 iTextEntry);
-
-        void DialogueUpdate(uint32 uiDiff);
-
-        void SetNewArray(DialogueEntry const* pDialogueArray) { m_pDialogueArray = pDialogueArray; }
-        void SetNewArray(DialogueEntryTwoSide const* aDialogueTwoSide) { m_pDialogueTwoSideArray = aDialogueTwoSide; }
-
-    protected:
-        /// Will be called when a dialogue step was done
-        virtual void JustDidDialogueStep(int32 /*iEntry*/) {}
-        /// Will be called to get a speaker, MUST be implemented if not used in instances
-        virtual Creature* GetSpeakerByEntry(uint32 /*uiEntry*/) { return nullptr; }
-
-    private:
-        void DoNextDialogueStep();
-
-        InstanceScript* m_pInstance;
-
-        DialogueEntry const* m_pDialogueArray;
-        DialogueEntry const* m_pCurrentEntry;
-        DialogueEntryTwoSide const* m_pDialogueTwoSideArray;
-        DialogueEntryTwoSide const* m_pCurrentEntryTwoSide;
-
-        uint32 m_uiTimer;
-        bool m_bIsFirstSide;
 };
 
 #endif
