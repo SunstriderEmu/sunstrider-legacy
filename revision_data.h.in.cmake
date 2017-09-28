f152c797320760ec4b9c6bb107f0095cf2c2d528
Kelno <kelnowm@gmail.com>
2017-09-28 12:55:39 +0200

    Updated base world sql file version
    Former-commit-id: fe007233187676466461fc02cee13c5a745036c0

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -7,7 +7,7 @@
  #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "dummy"
+ #define _FULL_DATABASE             "world_2017-09-28.sql"
  #define VER_COMPANYNAME_STR        "Sunstrider Project"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2016 Sunstrider"
  #define VER_FILEVERSION            0,0,0
