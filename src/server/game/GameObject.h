c5bb4caeb2c032f98512f20662d348f39a9093fe
Kelno <kelnowm@gmail.com>
2017-11-02 16:49:18 +0100

    Trigger an error and hack gameobject autoclose time to 1000ms when given autoclose time is invalid in db (there is none atm)
    Former-commit-id: 39d91053baaf1c5728ed82137f6ed59ba1d4dc5e

--- src/server/game/GameObject.h
+++ src/server/game/GameObject.h
@@ -758,7 +758,15 @@ class TC_GAME_API GameObject : public WorldObject, public GridObject<GameObject>
                 case GAMEOBJECT_TYPE_AREADAMAGE:    autoCloseTime = GetGOInfo()->areadamage.autoCloseTime; break;
                 default: break;
             }
-            return autoCloseTime / 0x10000;
+            closeTimeMS = autoCloseTime / 0x10000;
+            if (autoCloseTime != 0 && closeTimeMS == 0)
+            {
+                //sunstrider: a close time was specified but it was rounded down to 0... probably a scripting error, but let's at least return 1000ms so that the gameobject has an auto close time
+                closeTimeMS = 1000; 
+                TC_LOG_ERROR("entities.gameobject", "Gameobject %u has an auto close time but is too low (%u is going to be rounded down to 0 after calculation). Using 1000ms instead.", GetGOInfo()->entry, autoCloseTime);
+            }
+            return closeTimeMS;
+            //TC:             return autoCloseTime;              // prior to 3.0.3, conversion was / 0x10000;
         }
 
         void TriggeringLinkedGameObject( uint32 trapEntry, Unit* target);
