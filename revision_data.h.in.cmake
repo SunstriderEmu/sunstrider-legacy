bb8699b490841868b2f40ec17f4385ad636f3998
Kelno <kelnowm@gmail.com>
2017-11-03 21:43:09 +0100

    Updated world database release for AutoUpdater
    Former-commit-id: 1aef657e12104c6eac1c7ad42cbf513358d79ab8

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -7,9 +7,9 @@
  #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "world_2017-09-28.sql"
+ #define _FULL_DATABASE             "world_2017_11_03.sql"
  #define VER_COMPANYNAME_STR        "Sunstrider Project"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2016 Sunstrider"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2017 Sunstrider"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
