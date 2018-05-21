42327caa74f9cee1137dac16ba2b604d8e9492f2
Kelno <kelnowm@gmail.com>
2018-05-22 00:28:09 +0200

    Testing: Update BlackoutTestImpt comment
    Former-commit-id: a0355f5aee62d39cf2d44338ca867efc90387acf

--- src/server/scripts/Tests/spells/talents/talents_priest.cpp
+++ src/server/scripts/Tests/spells/talents/talents_priest.cpp
@@ -2133,7 +2133,7 @@ public:
     class BlackoutTestImpt : public TestCase
     {
     public:
-        BlackoutTestImpt() : TestCase(STATUS_WIP) { } // TODO Kelno: something's wrong with TEST_MELEE_PROC_CHANCE_CALLBACK
+        BlackoutTestImpt() : TestCase(STATUS_WIP) { } // Missing death plague and Shadowguard
 
         void Test() override
         {
@@ -2164,6 +2164,7 @@ public:
                 victim->ClearDiminishings();
             });
             //WoWWiki: Since Shadow damage is dealt to the target, Touch of Weakness will apply a charge of Shadow Weaving to the target, and it also has a chance to proc Blackout, making it a useful tool against Rogues, Warriors, Enhancement Shamans, etc.
+            //Bugged here, touch of weakness has no chance of proccing
             TEST_MELEE_PROC_CHANCE_CALLBACK(enemy, priest, spellProcId, true, procChance, MELEE_HIT_NORMAL, BASE_ATTACK, [](Unit* caster, Unit* victim) {
                 caster->ClearDiminishings();
                 victim->AddAura(ClassSpells::Priest::TOUCH_OF_WEAKNESS_RNK_7, victim);
