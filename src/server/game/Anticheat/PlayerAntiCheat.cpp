fd699b29dda56694fc55e2a5ede9018f02b5fec2
Kelno <kelnowm@gmail.com>
2018-07-16 13:57:02 +0200

    Added PlayerAntiCheat interface

---
+++ src/server/game/Anticheat/PlayerAntiCheat.cpp
@@ -0,0 +1,68 @@
+#include "PlayerAntiCheat.h"
+
+std::list<ModuleFactory*> PlayerAntiCheat::factories;
+
+void PlayerAntiCheat::RegisterModule(const string& /*name*/, ModuleFactory* m)
+{
+    factories.push_back(m);
+}
+
+PlayerAntiCheat::PlayerAntiCheat(WorldSession* session)
+{
+    for (auto& f : factories)
+        modules.push_back(std::unique_ptr<AntiCheatModule>(f->Create(session)));
+}
+
+void PlayerAntiCheat::OnClientPacketReceived(WorldPacket const& packet)
+{
+    for(auto& m : modules)
+        m->OnClientPacketReceived(packet);
+}
+
+void PlayerAntiCheat::OnPlayerMoved(Unit const* moved, MovementInfo const& movementInfo, OpcodeClient opcode)
+{
+    for (auto& m : modules)
+        m->OnPlayerMoved(moved, movementInfo, opcode);
+}
+
+void PlayerAntiCheat::OnPlayerSetFlying(Unit const* moved, bool set)
+{
+    for (auto& m : modules)
+        m->OnPlayerSetFlying(moved, set);
+}
+
+void PlayerAntiCheat::OnPlayerKnockBack(Unit const* moved)
+{
+    for (auto& m : modules)
+        m->OnPlayerKnockBack(moved);
+}
+
+void PlayerAntiCheat::OnPlayerTeleport(Unit const* moved)
+{
+    for (auto& m : modules)
+        m->OnPlayerTeleport(moved);
+}
+
+void PlayerAntiCheat::OnPlayerMoverChanged(Unit const* oldMover, Unit const* newMover)
+{
+    for (auto& m : modules)
+        m->OnPlayerMoverChanged(oldMover, newMover);
+}
+
+void PlayerAntiCheat::OnPlayerSpeedChanged(Unit const* moved, float oldSpeed, float newSpeed)
+{
+    for (auto& m : modules)
+        m->OnPlayerSpeedChanged(moved, oldSpeed, newSpeed);
+}
+
+void PlayerAntiCheat::OnPlayerWaterWalk(Unit const* moved)
+{
+    for (auto& m : modules)
+        m->OnPlayerWaterWalk(moved);
+}
+
+void PlayerAntiCheat::OnPlayerSlowfall(Unit const* moved)
+{
+    for (auto& m : modules)
+        m->OnPlayerSlowfall(moved);
+}
---
+++ src/server/game/Anticheat/PlayerAntiCheat.h
@@ -0,0 +1,67 @@
+#ifndef PLAYER_ANTICHEAT_H
+#define PLAYER_ANTICHEAT_H
+
+#include "Opcodes.h"
+
+class Unit;
+struct MovementInfo;
+class WorldSession;
+class WorldPacket;
+
+class AntiCheatModule
+{
+public:
+    virtual void OnClientPacketReceived(WorldPacket const& packet) {}
+    virtual void OnPlayerMoved(Unit const* moved, MovementInfo const& movementInfo, OpcodeClient opcode) {}
+    virtual void OnPlayerSetFlying(Unit const* moved, bool set) {}
+    virtual void OnPlayerKnockBack(Unit const* moved) {}
+    virtual void OnPlayerTeleport(Unit const* moved) {}
+    virtual void OnPlayerMoverChanged(Unit const* oldMover, Unit const* newMover) {}
+    virtual void OnPlayerSpeedChanged(Unit const* moved, float oldSpeed, float newSpeed) {}
+    virtual void OnPlayerWaterWalk(Unit const* moved) {}
+    virtual void OnPlayerSlowfall(Unit const* moved) {}
+};
+
+class ModuleFactory
+{
+public:
+    virtual AntiCheatModule* Create(WorldSession*) = 0;
+};
+
+class PlayerAntiCheat
+{
+public:
+    PlayerAntiCheat(WorldSession* session);
+
+    static void RegisterModule(const string& name, ModuleFactory* m);
+
+    void OnClientPacketReceived(WorldPacket const& packet);
+    void OnPlayerMoved(Unit const* moved, MovementInfo const& movementInfo, OpcodeClient opcode);
+    void OnPlayerSetFlying(Unit const* moved, bool set);
+    void OnPlayerKnockBack(Unit const* moved);
+    void OnPlayerTeleport(Unit const* moved);
+    void OnPlayerMoverChanged(Unit const* oldMover, Unit const* newMover);
+    void OnPlayerSpeedChanged(Unit const* moved, float oldSpeed, float newSpeed);
+    void OnPlayerWaterWalk(Unit const* moved);
+    void OnPlayerSlowfall(Unit const* moved);
+
+private:
+    std::list<std::unique_ptr<AntiCheatModule>> modules;
+
+    static std::list<ModuleFactory*> factories;
+};
+
+#define REGISTER_ANTICHEAT_MODULE(_class) \
+    class _class##Factory : public ModuleFactory { \
+    public: \
+        _class##Factory() \
+        { \
+            PlayerAntiCheat::RegisterModule(#_class, this); \
+        } \
+        virtual AntiCheatModule* Create(WorldSession* session) { \
+            return new _class(session); \
+        } \
+    }; \
+    static _class##Factory global_##_class##Factory;
+
+#endif
