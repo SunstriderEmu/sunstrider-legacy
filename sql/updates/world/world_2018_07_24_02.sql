52f60b35f6d539c03ae1544dcc20875cbb1ccc64
Kelno <kelnowm@gmail.com>
2018-07-24 20:20:22 +0200

    Spells: Fixed priest talent "Misery" being resistable

---
+++ sql/updates/world/world_2018_07_24_02.sql
@@ -0,0 +1,6 @@
+-- Added SPELL_ATTR3_IGNORE_HIT_RESULT to Shadow Priest Misery talent
+REPLACE INTO spell_template_override (entry, attributesEx3, comment) VALUES (33200, 0x00040000, "SPELL_ATTR3_IGNORE_HIT_RESULT");
+REPLACE INTO spell_template_override (entry, attributesEx3, comment) VALUES (33199, 0x00040000, "SPELL_ATTR3_IGNORE_HIT_RESULT");
+REPLACE INTO spell_template_override (entry, attributesEx3, comment) VALUES (33198, 0x00040000, "SPELL_ATTR3_IGNORE_HIT_RESULT");
+REPLACE INTO spell_template_override (entry, attributesEx3, comment) VALUES (33197, 0x00040000, "SPELL_ATTR3_IGNORE_HIT_RESULT");
+REPLACE INTO spell_template_override (entry, attributesEx3, comment) VALUES (33196, 0x00040000, "SPELL_ATTR3_IGNORE_HIT_RESULT");
