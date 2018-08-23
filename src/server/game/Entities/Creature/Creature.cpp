SunstriderEmu/sunstrider-legacy

commit e1525a3e5ed2b460b8b4fb10cbbe452e97da1d47

Ported cached queries system from TC

src/server/game/Entities/Creature/Creature.cpp
@@ -8,6 +8,7 @@
 #include "SpellMgr.h"
 #include "Creature.h"
 #include "QuestDef.h"
+#include "QueryPackets.h"
 #include "GossipDef.h"
 #include "Player.h"
 #include "Opcodes.h"
@@ -145,6 +146,61 @@ uint32 CreatureTemplate::GetFirstInvisibleModel() const
     */
 
     return 11686;
+}
+
+void CreatureTemplate::InitializeQueryData()
+{
+    for (uint8 loc = LOCALE_enUS; loc < TOTAL_LOCALES; ++loc)
+        QueryData[loc] = BuildQueryData(static_cast<LocaleConstant>(loc));
+}
+
+WorldPacket CreatureTemplate::BuildQueryData(LocaleConstant loc) const
+{
+    WorldPackets::Query::QueryCreatureResponse queryTemp;
+
+    std::string locName = Name, locTitle = Title;
+    if (CreatureLocale const* cl = sObjectMgr->GetCreatureLocale(Entry))
+    {
+        ObjectMgr::GetLocaleString(cl->Name, loc, locName);
+        ObjectMgr::GetLocaleString(cl->Title, loc, locTitle);
+    }
+
+    queryTemp.CreatureID = Entry;
+    queryTemp.Allow = true;
+
+    queryTemp.Stats.Name = locName;
+    queryTemp.Stats.NameAlt = locTitle;
+    queryTemp.Stats.CursorName = IconName;
+    queryTemp.Stats.Flags = type_flags;
+    queryTemp.Stats.CreatureType = type;
+    queryTemp.Stats.CreatureFamily = family;
+    queryTemp.Stats.Classification = rank;
+#ifdef LICH_KING
+    memcpy(queryTemp.Stats.ProxyCreatureID, KillCredit, sizeof(uint32) * MAX_KILL_CREDIT);
+#else
+    queryTemp.Stats.PetSpellDataId = PetSpellDataId;
+#endif
+    queryTemp.Stats.CreatureDisplayID[0] = Modelid1;
+    queryTemp.Stats.CreatureDisplayID[1] = Modelid2;
+    queryTemp.Stats.CreatureDisplayID[2] = Modelid3;
+    queryTemp.Stats.CreatureDisplayID[3] = Modelid4;
+    queryTemp.Stats.HpMulti = ModHealth;
+    queryTemp.Stats.EnergyMulti = ModMana;
+    queryTemp.Stats.Leader = RacialLeader;
+
+#ifdef LICH_KING
+    for (uint32 i = 0; i < MAX_CREATURE_QUEST_ITEMS; ++i)
+        queryTemp.Stats.QuestItems[i] = 0;
+
+    if (std::vector<uint32> const* items = sObjectMgr->GetCreatureQuestItemList(Entry))
+        for (uint32 i = 0; i < MAX_CREATURE_QUEST_ITEMS; ++i)
+            if (i < items->size())
+                queryTemp.Stats.QuestItems[i] = (*items)[i];
+
+    queryTemp.Stats.CreatureMovementInfoID = movementId;
+#endif
+    queryTemp.Write();
+    return queryTemp.Move();
 }
 
 CreatureBaseStats const* CreatureBaseStats::GetBaseStats(uint8 level, uint8 unitClass)

src/server/game/Server/Packets/QueryPackets.cpp
@@ -1,0 +1,230 @@
+/*
+ * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "QueryPackets.h"
+
+void WorldPackets::Query::QueryCreature::Read()
+{
+    _worldPacket >> CreatureID;
+#ifdef LICH_KING
+    _worldPacket >> Guid;
+#endif
+}
+
+WorldPacket const* WorldPackets::Query::QueryCreatureResponse::Write()
+{
+    _worldPacket << uint32(CreatureID | (Allow ? 0x00000000 : 0x80000000)); // creature entry
+
+    if (Allow)
+    {
+        _worldPacket << Stats.Name;
+        _worldPacket << uint8(0) << uint8(0) << uint8(0);                   // name2, name3, name4, always empty
+        _worldPacket << Stats.NameAlt;
+        _worldPacket << Stats.CursorName;                                   // "Directions" for guard, string for Icons 2.3.0
+        _worldPacket << uint32(Stats.Flags);                                // flags
+        _worldPacket << uint32(Stats.CreatureType);                         // CreatureType.dbc
+        _worldPacket << uint32(Stats.CreatureFamily);                       // CreatureFamily.dbc
+        _worldPacket << uint32(Stats.Classification);                       // Creature Rank (elite, boss, etc)
+#ifdef LICH_KING
+        _worldPacket.append(Stats.ProxyCreatureID, MAX_KILL_CREDIT);        // new in 3.1, kill credit
+#else
+        _worldPacket << uint32(0);                                          // unknown        wdbFeild11
+        _worldPacket << uint32(Stats.PetSpellDataId);                       // Id from CreatureSpellData.dbc    wdbField12
+#endif
+        _worldPacket.append(Stats.CreatureDisplayID, MAX_CREATURE_MODELS);  // Modelid
+        _worldPacket << float(Stats.HpMulti);                               // dmg/hp modifier
+        _worldPacket << float(Stats.EnergyMulti);                           // dmg/mana modifier
+        _worldPacket << uint8(Stats.Leader);
+#ifdef LICH_KING
+        _worldPacket.append(Stats.QuestItems, MAX_CREATURE_QUEST_ITEMS);
+        _worldPacket << uint32(Stats.CreatureMovementInfoID);               // CreatureMovementInfo.dbc
+#endif
+    }
+
+    return &_worldPacket;
+}
+
+void WorldPackets::Query::QueryGameObject::Read()
+{
+    _worldPacket >> GameObjectID;
+#ifdef LICH_KING
+    _worldPacket >> Guid;
+#endif
+}
+
+WorldPacket const* WorldPackets::Query::QueryGameObjectResponse::Write()
+{
+    _worldPacket << uint32(GameObjectID | (Allow ? 0x00000000 : 0x80000000));
+
+    if (Allow)
+    {
+        _worldPacket << uint32(Stats.Type);
+        _worldPacket << uint32(Stats.DisplayID);
+        _worldPacket << Stats.Name;
+        _worldPacket << uint8(0) << uint8(0) << uint8(0);           // name2, name3, name4
+        _worldPacket << Stats.IconName;                             // 2.0.3, string. Icon name to use instead of default icon for go's (ex: "Attack" makes sword)
+        _worldPacket << Stats.CastBarCaption;                       // 2.0.3, string. Text will appear in Cast Bar when using GO (ex: "Collecting")
+        _worldPacket << Stats.UnkString;                            // 2.0.3, string
+        _worldPacket.append(Stats.Data, MAX_GAMEOBJECT_DATA);
+        _worldPacket << float(Stats.Size);                          // go size
+#ifdef LICH_KING
+        _worldPacket.append(Stats.QuestItems, MAX_GAMEOBJECT_QUEST_ITEMS);
+#endif
+    }
+
+    return &_worldPacket;
+}
+
+void WorldPackets::Query::QueryItemSingle::Read()
+{
+    _worldPacket >> ItemID;
+}
+
+WorldPacket const* WorldPackets::Query::QueryItemSingleResponse::Write()
+{
+    _worldPacket << uint32(ItemID | (Allow ? 0x00000000 : 0x80000000));
+
+    if (Allow)
+    {
+        _worldPacket << Stats.Class;
+        _worldPacket << Stats.SubClass;
+        _worldPacket << Stats.SoundOverrideSubclass;
+        _worldPacket << Stats.Name;
+        _worldPacket << uint8(0x00);                              //Name2; // blizz not send name there, just uint8(0x00); <-- \0 = empty string = empty name...
+        _worldPacket << uint8(0x00);                              //Name3; // blizz not send name there, just uint8(0x00);
+        _worldPacket << uint8(0x00);                              //Name4; // blizz not send name there, just uint8(0x00);
+        _worldPacket << Stats.DisplayInfoID;
+        _worldPacket << Stats.Quality;
+        _worldPacket << Stats.Flags;
+#ifdef LICH_KING
+        _worldPacket << Stats.Flags2;
+#endif
+        _worldPacket << Stats.BuyPrice;
+        _worldPacket << Stats.SellPrice;
+        _worldPacket << Stats.InventoryType;
+        _worldPacket << Stats.AllowableClass;
+        _worldPacket << Stats.AllowableRace;
+        _worldPacket << Stats.ItemLevel;
+        _worldPacket << Stats.RequiredLevel;
+        _worldPacket << Stats.RequiredSkill;
+        _worldPacket << Stats.RequiredSkillRank;
+        _worldPacket << Stats.RequiredSpell;
+        _worldPacket << Stats.RequiredHonorRank;
+        _worldPacket << Stats.RequiredCityRank;
+        _worldPacket << Stats.RequiredReputationFaction;
+        _worldPacket << Stats.RequiredReputationRank;
+        _worldPacket << int32(Stats.MaxCount);
+        _worldPacket << int32(Stats.Stackable);
+        _worldPacket << Stats.ContainerSlots;
+        _worldPacket << Stats.StatsCount;                         // item stats count
+        for (uint32 i = 0; i < Stats.StatsCount; ++i)
+        {
+            _worldPacket << Stats.ItemStat[i].ItemStatType;
+            _worldPacket << Stats.ItemStat[i].ItemStatValue;
+        }
+#ifdef LICH_KING
+        _worldPacket << Stats.ScalingStatDistribution;            // scaling stats distribution
+        _worldPacket << Stats.ScalingStatValue;                   // some kind of flags used to determine stat values column
+#endif
+        for (uint8 i = 0; i < MAX_ITEM_PROTO_DAMAGES; ++i)
+        {
+            _worldPacket << Stats.Damage[i].DamageMin;
+            _worldPacket << Stats.Damage[i].DamageMax;
+            _worldPacket << Stats.Damage[i].DamageType;
+        }
+
+        // resistances (7)
+        for (uint8 i = SPELL_SCHOOL_NORMAL; i < MAX_SPELL_SCHOOL; ++i)
+            _worldPacket << Stats.Resistance[i];
+
+        _worldPacket << Stats.Delay;
+        _worldPacket << Stats.AmmoType;
+        _worldPacket << Stats.RangedModRange;
+
+        for (uint8 s = 0; s < MAX_ITEM_PROTO_SPELLS; ++s)
+        {
+            // spells are validated on template loading
+            if (Stats.Spells[s].SpellId > 0)
+            {
+                _worldPacket << Stats.Spells[s].SpellId;
+                _worldPacket << Stats.Spells[s].SpellTrigger;
+                _worldPacket << uint32(-abs(Stats.Spells[s].SpellCharges));
+                _worldPacket << uint32(Stats.Spells[s].SpellCooldown);
+                _worldPacket << uint32(Stats.Spells[s].SpellCategory);
+                _worldPacket << uint32(Stats.Spells[s].SpellCategoryCooldown);
+            }
+            else
+            {
+                _worldPacket << uint32(0);
+                _worldPacket << uint32(0);
+                _worldPacket << uint32(0);
+                _worldPacket << uint32(-1);
+                _worldPacket << uint32(0);
+                _worldPacket << uint32(-1);
+            }
+        }
+        _worldPacket << Stats.Bonding;
+        _worldPacket << Stats.Description;
+        _worldPacket << Stats.PageText;
+        _worldPacket << Stats.LanguageID;
+        _worldPacket << Stats.PageMaterial;
+        _worldPacket << Stats.StartQuest;
+        _worldPacket << Stats.LockID;
+        _worldPacket << int32(Stats.Material);
+        _worldPacket << Stats.Sheath;
+        _worldPacket << Stats.RandomProperty;
+        _worldPacket << Stats.RandomSuffix;
+        _worldPacket << Stats.Block;
+        _worldPacket << Stats.ItemSet;
+        _worldPacket << Stats.MaxDurability;
+        _worldPacket << Stats.Area;
+        _worldPacket << Stats.Map;                                // Added in 1.12.x & 2.0.1 client branch
+        _worldPacket << Stats.BagFamily;
+        _worldPacket << Stats.TotemCategory;
+        for (uint8 s = 0; s < MAX_ITEM_PROTO_SOCKETS; ++s)
+        {
+            _worldPacket << Stats.Socket[s].Color;
+            _worldPacket << Stats.Socket[s].Content;
+        }
+        _worldPacket << Stats.SocketBonus;
+        _worldPacket << Stats.GemProperties;
+        _worldPacket << Stats.RequiredDisenchantSkill;
+        _worldPacket << Stats.ArmorDamageModifier;
+        _worldPacket << Stats.Duration;                           // added in 2.4.2.8209, duration (seconds)
+#ifdef LICH_KING
+        _worldPacket << Stats.ItemLimitCategory;                  // WotLK, ItemLimitCategory
+        _worldPacket << Stats.HolidayId;                          // Holiday.dbc?
+#endif
+    }
+
+    return &_worldPacket;
+}
+
+#ifdef LICH_KING
+void WorldPackets::Query::QuestPOIQuery::Read()
+{
+    _worldPacket >> MissingQuestCount; // quest count, max=25
+
+    if (MissingQuestCount <= MAX_QUEST_LOG_SIZE)
+    {
+        for (uint8 i = 0; i < MissingQuestCount; ++i)
+            _worldPacket >> MissingQuestPOIs[i];
+    }
+
+    _worldPacket.rfinish();
+}
+#endif

src/server/game/CMakeLists.txt
@@ -18,7 +18,7 @@ CollectSourceFiles(
   ${EXCLUDE_PLAYERBOT}
   ${EXCLUDE_TESTS}
 )
-
+ 
 set(PRIVATE_PCH_HEADER PrecompiledHeaders/gamePCH.h)
 
 GroupSources(${CMAKE_CURRENT_SOURCE_DIR})

src/server/game/Entities/Creature/Creature.h
@@ -4,11 +4,12 @@
 
 #include "Common.h"
 #include "Unit.h"
-#include "ItemPrototype.h"
+#include "ItemTemplate.h"
 #include "LootMgr.h"
 #include "CreatureGroups.h"
 #include "Duration.h"
 #include "Loot.h"
+#include "WorldPacket.h"
 
 #include <list>
 #include <string>
@@ -210,7 +211,7 @@ struct CreatureTemplate
     uint32  Modelid3;
     uint32  Modelid4;
     std::string Name;
-    std::string SubName;
+    std::string Title;
     std::string IconName;
     uint32  GossipMenuId;
     uint32  minlevel;
@@ -260,6 +261,7 @@ struct CreatureTemplate
     uint32  flags_extra;
     uint32  ScriptID;
     uint32  QuestPoolId;
+    WorldPacket QueryData[TOTAL_LOCALES];
     uint32 GetRandomValidModelId() const;
     uint32 GetFirstValidModelId() const;
     uint32  GetFirstInvisibleModel() const;
@@ -292,6 +294,9 @@ struct CreatureTemplate
         // if can tame exotic then can tame any tameable
         return canTameExotic || !IsExotic();
     }
+
+    void InitializeQueryData();
+    WorldPacket BuildQueryData(LocaleConstant loc) const;
 };
 
 typedef std::unordered_map<uint32, CreatureTemplate> CreatureTemplateContainer;
@@ -340,7 +345,7 @@ typedef std::unordered_map<uint16, CreatureBaseStats> CreatureBaseStatsContainer
 struct CreatureLocale
 {
     std::vector<std::string> Name;
-    std::vector<std::string> SubName;
+    std::vector<std::string> Title;
 };
 
 struct GossipMenuItemsLocale
@@ -544,7 +549,7 @@ class TC_GAME_API Creature : public Unit, public GridObject<Creature>, public Ma
         bool HasMainWeapon() const override;
 
         uint32 GetSpawnId() const { return m_spawnId; }
-        std::string const& GetSubName() const { return GetCreatureTemplate()->SubName; }
+        std::string const& GetTitle() const { return GetCreatureTemplate()->Title; }
 
         void Update( uint32 time ) override;
         void GetRespawnPosition(float &x, float &y, float &z, float* ori = nullptr, float* dist =nullptr) const;

src/server/game/Entities/Creature/GossipDef.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include "QuestDef.h"
 #include "GossipDef.h"
@@ -23,6 +6,7 @@
 #include "WorldPacket.h"
 #include "WorldSession.h"
 #include "Formulas.h"
+#include "QuestPackets.h"
 
 GossipMenu::GossipMenu()
 {
@@ -439,10 +423,10 @@ void PlayerMenu::SendQuestGiverStatus(uint8 questStatus, ObjectGuid npcGUID) con
 
 void PlayerMenu::SendQuestGiverQuestDetails(Quest const* quest, ObjectGuid npcGUID, bool activateAccept) const
 {
-    std::string questTitle      = quest->GetTitle();
-    std::string questDetails    = quest->GetDetails();
-    std::string questObjectives = quest->GetObjectives();
-    std::string questEndText    = quest->GetEndText();
+    std::string questTitle           = quest->GetTitle();
+    std::string questDetails         = quest->GetDetails();
+    std::string questObjectives      = quest->GetObjectives();
+    std::string questAreaDescription = quest->GetAreaDescription();
 
     int32 locale = _session->GetSessionDbLocaleIndex();
     if (locale >= 0)
@@ -452,12 +436,9 @@ void PlayerMenu::SendQuestGiverQuestDetails(Quest const* quest, ObjectGuid npcGU
             ObjectMgr::GetLocaleString(localeData->Title, locale, questTitle);
             ObjectMgr::GetLocaleString(localeData->Details, locale, questDetails);
             ObjectMgr::GetLocaleString(localeData->Objectives, locale, questObjectives);
-            ObjectMgr::GetLocaleString(localeData->EndText, locale, questEndText);
+            ObjectMgr::GetLocaleString(localeData->AreaDescription, locale, questAreaDescription);
         }
     }
-
-  /*  if (sWorld->GetConfig(CONFIG_UI_QUESTLEVELS_IN_DIALOGS))
-        AddQuestLevelToTitle(questTitle, quest->GetQuestLevel());*/
 
     WorldPacket data(SMSG_QUESTGIVER_QUEST_DETAILS, 100);   // guess size
     data << uint64(npcGUID);
@@ -574,178 +555,22 @@ void PlayerMenu::SendQuestGiverQuestDetails(Quest const* quest, ObjectGuid npcGU
 
 void PlayerMenu::SendQuestQueryResponse(Quest const* quest) const
 {
-    std::string questTitle = quest->GetTitle();
-    std::string questDetails = quest->GetDetails();
-    std::string questObjectives = quest->GetObjectives();
-    std::string questEndText = quest->GetEndText();
-#ifdef LICH_KING
-    std::string questCompletedText = quest->GetCompletedText();
-#endif
-
-    std::string questObjectiveText[QUEST_OBJECTIVES_COUNT];
-    for (uint8 i = 0; i < QUEST_OBJECTIVES_COUNT; ++i)
-        questObjectiveText[i] = quest->ObjectiveText[i];
-
-    int32 locale = _session->GetSessionDbLocaleIndex();
-    if (locale >= 0)
-    {
-        if (QuestLocale const* localeData = sObjectMgr->GetQuestLocale(quest->GetQuestId()))
-        {
-            ObjectMgr::GetLocaleString(localeData->Title, locale, questTitle);
-            ObjectMgr::GetLocaleString(localeData->Details, locale, questDetails);
-            ObjectMgr::GetLocaleString(localeData->Objectives, locale, questObjectives);
-            ObjectMgr::GetLocaleString(localeData->EndText, locale, questEndText);
-#ifdef LICH_KING
-            ObjectMgr::GetLocaleString(localeData->CompletedText, locale, questCompletedText);
-#endif
-
-            for (uint8 i = 0; i < QUEST_OBJECTIVES_COUNT; ++i)
-                ObjectMgr::GetLocaleString(localeData->ObjectiveText[i], locale, questObjectiveText[i]);
-        }
-    }
-
-    WorldPacket data(SMSG_QUEST_QUERY_RESPONSE, 100);       // guess size
-
-    data << uint32(quest->GetQuestId());                    // quest id
-    data << uint32(quest->GetQuestMethod());                // Accepted values: 0, 1 or 2. 0 == IsAutoComplete() (skip objectives/details)
-    data << uint32(quest->GetQuestLevel());                 // may be -1, static data, in other cases must be used dynamic level: Player::GetQuestLevel (0 is not known, but assuming this is no longer valid for quest intended for client)
-#ifdef LICH_KING
-    data << uint32(quest->GetMinLevel());                   // min level
-#endif
-
-    data << uint32(quest->GetZoneOrSort());                 // zone or sort to display in quest log
-
-    data << uint32(quest->GetType());                       // quest type
-    data << uint32(quest->GetSuggestedPlayers());           // suggested players count
-
-    data << uint32(quest->GetRepObjectiveFaction());        // shown in quest log as part of quest objective
-    data << uint32(quest->GetRepObjectiveValue());          // shown in quest log as part of quest objective
-    
-#ifdef LICH_KING
-    data << uint32(quest->GetRepObjectiveFaction2());       // shown in quest log as part of quest objective OPPOSITE faction
-    data << uint32(quest->GetRepObjectiveValue2());         // shown in quest log as part of quest objective OPPOSITE faction
-#else
-    data << uint32(0); //always 0 on BC
-    data << uint32(0); //always 0 on BC
-#endif
-
-    data << uint32(quest->GetNextQuestInChain());           // client will request this quest from NPC, if not 0
-
-#ifdef LICH_KING
-    // data << uint32(quest->GetXPId());                       // used for calculating rewarded experience
-    data << uint32(0);
-#endif
-
-    if (quest->HasFlag(QUEST_FLAGS_HIDDEN_REWARDS))
-        data << uint32(0);                                  // Hide money rewarded
+    WorldPacket queryPacket;
+    //sun: copy packet instead of directly using it, because we modify it below
+    if (sWorld->getBoolConfig(CONFIG_CACHE_DATA_QUERIES))
+        queryPacket = quest->QueryData[static_cast<uint32>(_session->GetSessionDbLocaleIndex())];
     else
-        data << uint32(quest->GetRewOrReqMoney());          // reward money (below max lvl)
+        queryPacket = quest->BuildQueryData(_session->GetSessionDbLocaleIndex());
 
-    data << uint32(quest->GetRewMoneyMaxLevel());           // used in XP calculation at client
-    data << uint32(quest->GetRewSpell());                   // reward spell, this spell will display (icon) (cast if RewardSpellCast == 0)
-    data << uint32(quest->GetRewSpellCast());                // cast spell
-
-    // rewarded honor points
-    data << uint32(Trinity::Honor::hk_honor_at_level(_session->GetPlayer()->GetLevel(), quest->GetRewHonorableKills()));// TrinityCore : data << uint32(quest->GetRewHonorAddition());
-    if(_session->GetClientBuild() == BUILD_335)
-    {
-        //TODO LK data << float(quest->GetRewHonorMultiplier());
-         data << uint32(0);
-    }
-    data << uint32(quest->GetSrcItemId());                  // source item id
-    data << uint32(quest->GetFlags() & 0xFFFF);             // quest flags
-    data << uint32(quest->GetCharTitleId());                // CharTitleId, new 2.4.0, player gets this title (id from CharTitles)
-#ifdef BUILD_335_SUPPORT
-    if(_session->GetClientBuild() == BUILD_335)
-    {
-       /* data << uint32(quest->GetPlayersSlain());               // players slain
-        data << uint32(quest->GetBonusTalents());               // bonus talents
-        data << uint32(quest->GetRewArenaPoints());             // bonus arena points */
-        data << uint32(0);
-        data << uint32(0);
-        data << uint32(0);
-
-        data << uint32(0);                                      // review rep show mask
-    }
+#ifndef LICH_KING
+    // Workaround for BC. The query system is designed for static paquets but this info is not static because 
+    // the field RewardHonorForPlayer depends on the player level.
+    size_t offset = WorldPackets::Quest::QueryQuestInfoResponse::RewardHonorForPlayerPos;
+    queryPacket.put<uint32>(offset, Trinity::Honor::hk_honor_at_level(_session->GetPlayer()->GetLevel(), queryPacket.read<uint32>(offset)));
 #endif
 
-    if (quest->HasFlag(QUEST_FLAGS_HIDDEN_REWARDS))
-    {
-        for (uint8 i = 0; i < QUEST_REWARDS_COUNT; ++i)
-            data << uint32(0) << uint32(0);
-        for (uint8 i = 0; i < QUEST_REWARD_CHOICES_COUNT; ++i)
-            data << uint32(0) << uint32(0);
-    }
-    else
-    {
-        for (uint8 i = 0; i < QUEST_REWARDS_COUNT; ++i)
-        {
-            data << uint32(quest->RewardItemId[i]);
-            data << uint32(quest->RewardItemIdCount[i]);
-        }
-        for (uint8 i = 0; i < QUEST_REWARD_CHOICES_COUNT; ++i)
-        {
-            data << uint32(quest->RewardChoiceItemId[i]);
-            data << uint32(quest->RewardChoiceItemCount[i]);
-        }
-    }
+    _session->SendPacket(&queryPacket);
 
-#ifdef LICH_KING
-    for (uint8 i = 0; i < QUEST_REPUTATIONS_COUNT; ++i)        // reward factions ids
-        data << uint32(quest->RewardFactionId[i]);
-
-    for (uint8 i = 0; i < QUEST_REPUTATIONS_COUNT; ++i)        // columnid+1 QuestFactionReward.dbc?
-        data << int32(quest->RewardFactionValueId[i]);
-
-    for (uint8 i = 0; i < QUEST_REPUTATIONS_COUNT; ++i)        // unk (0)
-        data << int32(quest->RewardFactionValueIdOverride[i]);
-#endif
-
-    data << uint32(quest->GetPointMapId());
-    data << float(quest->GetPointX());
-    data << float(quest->GetPointY());
-    data << uint32(quest->GetPointOpt());
-
-#ifdef LICH_KING
-    if (sWorld->getBoolConfig(CONFIG_UI_QUESTLEVELS_IN_DIALOGS))
-        AddQuestLevelToTitle(questTitle, quest->GetQuestLevel());
-#endif
-
-    data << questTitle;
-    data << questObjectives;
-    data << questDetails;
-    data << questEndText;
-#ifdef LICH_KING
-    data << questCompletedText;                                 // display in quest objectives window once all objectives are completed
-#endif
-
-    for (uint8 i = 0; i < QUEST_OBJECTIVES_COUNT; ++i)
-    {
-        if (quest->RequiredNpcOrGo[i] < 0)
-            data << uint32((quest->RequiredNpcOrGo[i] * (-1)) | 0x80000000);    // client expects gameobject template id in form (id|0x80000000)
-        else
-            data << uint32(quest->RequiredNpcOrGo[i]);
-
-        data << uint32(quest->RequiredNpcOrGoCount[i]);
-        data << uint32(quest->RequiredItemId[i]);
-        data << uint32(quest->RequiredItemCount[i]); 
-        /* Trinity core for the last two ones.
-        data << uint32(quest->ItemDrop[i]);
-        data << uint32(0);                                  // req source count?
-        */
-    }
-
-    if(_session->GetClientBuild() == BUILD_335)
-        for (uint8 i = 0; i < QUEST_ITEM_OBJECTIVES_COUNT; ++i)
-        {
-            data << uint32(quest->RequiredItemId[i]);
-            data << uint32(quest->RequiredItemCount[i]);
-        }
-
-    for (auto & i : questObjectiveText)
-        data << i;
-
-    _session->SendPacket(&data);
     TC_LOG_DEBUG("network", "WORLD: Sent SMSG_QUEST_QUERY_RESPONSE questid=%u", quest->GetQuestId());
 }
 
@@ -763,9 +588,6 @@ void PlayerMenu::SendQuestGiverOfferReward(Quest const* quest, ObjectGuid npcGUI
             ObjectMgr::GetLocaleString(localeData->_offerRewardText, locale, questOfferRewardText);
         }
     }
-
-  /* TrinityCore  if (sWorld->Config(CONFIG_UI_QUESTLEVELS_IN_DIALOGS))
-        AddQuestLevelToTitle(questTitle, quest->GetQuestLevel()); */
 
     WorldPacket data(SMSG_QUESTGIVER_OFFER_REWARD, 50);     // guess size
     data << uint64(npcGUID);
@@ -959,3 +781,4 @@ void PlayerMenu::AddQuestLevelToTitle(std::string &title, int32 level)
     questTitlePretty << "[" << level << "] " << title;
     title = questTitlePretty.str();
 }
+

commit 6b9c3a1af81bb63365e919465b634d90be016abc

Fixed CMSG_ITEM_QUERY_SINGLE structure (broken in e1525a3e5ed2b460b8b4fb10cbbe452e97da1d47)

src/server/game/Entities/Creature/Creature.cpp
@@ -1393,7 +1393,7 @@ bool Creature::isTappedBy(Player const* player) const
     return true;
 }
 
-void Creature::SetLootRecipient(Unit *unit, bool withGroup)
+void Creature::SetLootRecipient(Unit* unit, bool withGroup)
 {
     // set the player whose group should receive the right
     // to loot the creature after it dies

src/server/game/Entities/Player/Player.cpp
@@ -11524,7 +11524,7 @@ void Player::QuickEquipItem( uint16 pos, Item *pItem)
     }
 }
 
-void Player::SetVisibleItemSlot(uint8 slot, Item *pItem)
+void Player::SetVisibleItemSlot(uint8 slot, Item* pItem)
 {
     // PLAYER_VISIBLE_ITEM_i_CREATOR    // Size: 2
     // PLAYER_VISIBLE_ITEM_i_0          // Size: 12

src/server/game/Server/Packets/QueryPackets.cpp
@@ -130,8 +130,12 @@ WorldPacket const* WorldPackets::Query::QueryItemSingleResponse::Write()
         _worldPacket << int32(Stats.MaxCount);
         _worldPacket << int32(Stats.Stackable);
         _worldPacket << Stats.ContainerSlots;
+#ifdef LICH_KING
         _worldPacket << Stats.StatsCount;                         // item stats count
         for (uint32 i = 0; i < Stats.StatsCount; ++i)
+#else
+        for (uint32 i = 0; i < MAX_ITEM_PROTO_STATS; ++i)
+#endif
         {
             _worldPacket << Stats.ItemStat[i].ItemStatType;
             _worldPacket << Stats.ItemStat[i].ItemStatValue;