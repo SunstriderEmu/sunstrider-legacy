e151e756ad83185d51d6e1cc774b3bd0acc20a4d
Kelno <kelnowm@gmail.com>
2018-08-26 18:18:11 +0200

    [TC] Core/Maps: initialize PositionFullTerrainStatus to avoid having issues with those variables (TrinityCore/#22315)

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -96,6 +96,7 @@ struct PositionFullTerrainStatus
         uint32 const mogpFlags;
     };
 
+    PositionFullTerrainStatus() : areaId(0), floorZ(0.0f), outdoors(true)
     uint32 areaId;
     float floorZ;
     bool outdoors;
