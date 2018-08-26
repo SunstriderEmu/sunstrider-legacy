c9091d1692f3a0776e01afe8e85f2d6b74937b18
Kelno <kelnowm@gmail.com>
2018-08-26 21:36:47 +0200

    Added some missing command in command table

---
+++ sql/updates/world/world_2018_08_26_05.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO command VALUES ("list auras", 1, "");
+INSERT IGNORE INTO command VALUES ("pet happy", 1, "");
+INSERT IGNORE INTO command VALUES ("updatetitle", 1, "");
+INSERT IGNORE INTO command VALUES ("shop", 1, "");
