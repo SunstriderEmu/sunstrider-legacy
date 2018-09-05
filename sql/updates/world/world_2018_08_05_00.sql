37ea0787115f2bf8aca559fe1734a942b9c77937
Kelno <kelnowm@gmail.com>
2018-09-06 00:17:23 +0200

    Fixed last commit

---
+++ sql/updates/world/world_2018_08_05_00.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `updates` (`name`, `hash`, `state`, `timestamp`, `speed`) VALUES
+	('world_2018_08_05_01.sql', '6C7B9EE7158BA9D07C24E951BCCFD3B3A3570FBD', 'RELEASED', '2018-09-05 20:07:02', 378);
