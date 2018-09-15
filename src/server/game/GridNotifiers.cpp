70a4ee75cb5938addcd393f48dc0fe0534eee629
Kelno <kelnowm@gmail.com>
2018-09-15 17:06:05 +0200

    Fixed NearestAttackableUnitInObjectRangeCheck. Fixes #35

--- src/server/game/GridNotifiers.cpp
+++ src/server/game/GridNotifiers.cpp
@@ -457,7 +457,7 @@ bool AnyUnitInObjectRangeCheck::operator()(Unit* u)
 
 bool NearestAttackableUnitInObjectRangeCheck::operator()(Unit* u)
 {
-    if (i_funit->IsValidAttackTarget(u) == CAN_ATTACK_RESULT_OK && i_obj->IsWithinDistInMap(u, i_range) &&
+    if (i_funit->IsValidAttackTarget(u) && i_obj->IsWithinDistInMap(u, i_range) &&
         (i_funit->IsInCombatWith(u) || i_funit->IsHostileTo(u)) && i_obj->CanSeeOrDetect(u))
     {
         i_range = i_obj->GetDistance(u);        // use found unit range as new range limit for next check
