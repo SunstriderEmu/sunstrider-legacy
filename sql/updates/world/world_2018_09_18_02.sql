ae454b0828940e09f828be79036f58bdd97f9a9c
Kelno <kelnowm@gmail.com>
2018-09-18 16:18:12 +0200

    DB: Added missing comment to last commit

--- sql/updates/world/world_2018_09_18_02.sql
+++ sql/updates/world/world_2018_09_18_02.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `creature_entry`   
-  CHANGE `equipment_id` `equipment_id` TINYINT(1) DEFAULT -1  NOT NULL;
+  CHANGE `equipment_id` `equipment_id` TINYINT(1) DEFAULT -1  NOT NULL  COMMENT '-1: random, 0: no equipment, >0 equipment id';
