SunstriderEmu/sunstrider-legacy

commit c3fcd8aa29613fec428752ebe6c3351113ebe496

(Based on xvwyh work) Incorrect calculation of the first "fake" Catmull-Rom spline point

src/server/game/Movement/Spline/Spline.cpp
@@ -199,23 +199,23 @@ float SplineBase::SegLengthBezier3(index_type index) const
     return length;
 }
 
-void SplineBase::init_spline(const Vector3 * controls, index_type count, EvaluationMode m)
+void SplineBase::init_spline(const Vector3 * controls, index_type count, EvaluationMode m, float initialOrientation)
 {
     m_mode = m;
     cyclic = false;
 
-    (this->*initializers[m_mode])(controls, count, cyclic, 0);
+    (this->*initializers[m_mode])(controls, count, cyclic, 0, initialOrientation);
 }
 
-void SplineBase::init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point)
+void SplineBase::init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point, float initialOrientation)
 {
     m_mode = m;
     cyclic = true;
 
-    (this->*initializers[m_mode])(controls, count, cyclic, cyclic_point);
+    (this->*initializers[m_mode])(controls, count, cyclic, cyclic_point, initialOrientation);
 }
 
-void SplineBase::InitLinear(const Vector3* controls, index_type count, bool localCyclic, index_type cyclic_point)
+void SplineBase::InitLinear(const Vector3* controls, index_type count, bool localCyclic, index_type cyclic_point, float /*initialOrientation*/)
 {
     ASSERT(count >= 2);
     const int real_size = count + 1;
@@ -235,7 +235,7 @@ void SplineBase::InitLinear(const Vector3* controls, index_type count, bool loca
     index_hi = localCyclic ? count : (count - 1);
 }
 
-void SplineBase::InitCatmullRom(const Vector3* controls, index_type count, bool localCyclic, index_type cyclic_point)
+void SplineBase::InitCatmullRom(const Vector3* controls, index_type count, bool localCyclic, index_type cyclic_point, float initialOrientation)
 {
     const int real_size = count + (localCyclic ? (1+2) : (1+1));
 
@@ -247,28 +247,27 @@ void SplineBase::InitCatmullRom(const Vector3* controls, index_type count, bool 
     memcpy(&points[lo_index], controls, sizeof(Vector3) * count);
 
     // first and last two indexes are space for special 'virtual points'
-    // these points are required for proper C_Evaluate and C_Evaluate_Derivative methtod work
+    // these points are required for proper C_Evaluate and C_Evaluate_Derivative method work
     if (localCyclic)
     {
         if (cyclic_point == 0)
             points[0] = controls[count-1];
         else
-            points[0] = controls[0].lerp(controls[1], -1);
+            points[0] = controls[0] - G3D::Vector3{ cos(initialOrientation), sin(initialOrientation), 0.0f }; // sun: from TrinityCore/TrinityCore/issues/22435
 
         points[high_index+1] = controls[cyclic_point];
         points[high_index+2] = controls[cyclic_point+1];
     }
     else
     {
-        points[0] = controls[0].lerp(controls[1], -1);
-        points[high_index+1] = controls[count-1];
+        points[0] = controls[0] - G3D::Vector3{ cos(initialOrientation), sin(initialOrientation), 0.0f }; // sun: from TrinityCore/TrinityCore/issues/22435
     }
 
     index_lo = lo_index;
     index_hi = high_index + (cyclic ? 1 : 0);
 }
 
-void SplineBase::InitBezier3(const Vector3* controls, index_type count, bool /*cyclic*/, index_type /*cyclic_point*/)
+void SplineBase::InitBezier3(const Vector3* controls, index_type count, bool /*cyclic*/, index_type /*cyclic_point*/, float /*initialOrientation*/)
 {
     index_type c = count / 3u * 3u;
     index_type t = c / 3u;

src/server/game/Maps/TransportMgr.cpp
@@ -217,8 +217,8 @@ void TransportMgr::GeneratePath(GameObjectTemplate const* goInfo, TransportTempl
         if (keyFrames[i - 1].Teleport || i + 1 == keyFrames.size())
         {
             size_t extra = !keyFrames[i - 1].Teleport ? 1 : 0;
-            auto  spline = new TransportSpline();
-            spline->init_spline(&splinePath[start], i - start + extra, Movement::SplineBase::ModeCatmullrom);
+            auto spline = new TransportSpline();
+            spline->init_spline(&splinePath[start], i - start + extra, Movement::SplineBase::ModeCatmullrom, keyFrames[0].InitialOrientation); //not sure about this initial orientation
             spline->initLengths();
             for (size_t j = start; j < i + extra; ++j)
             {

src/server/game/Movement/Spline/MoveSpline.cpp
@@ -134,11 +134,11 @@ void MoveSpline::init_spline(const MoveSplineInitArgs& args)
         // MoveSplineFlag::Enter_Cycle support dropped
         //if (splineflags & SPLINEFLAG_ENTER_CYCLE)
         //cyclic_point = 1;   // shouldn't be modified, came from client
-        spline.init_cyclic_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()], cyclic_point);
+        spline.init_cyclic_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()], cyclic_point, args.initialOrientation);
     }
     else
     {
-        spline.init_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()]);
+        spline.init_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()], args.initialOrientation);
     }
 
     // init spline timestamps

src/server/game/Movement/Spline/MovementPacketBuilder.cpp
@@ -55,6 +55,9 @@ namespace Movement
         }
 
         // add fake Enter_Cycle flag - needed for client-side cyclic movement (client will erase first spline vertex after first cycle done)
+        // also see: https://github.com/TrinityCore/TrinityCore/issues/22448
+        // Apparently MoveSplineFlag::Enter_Cycle tells the client to discord the first point so that may help
+        // Also SMSG_FLIGHT_SPLINE_SYNC must be used
         splineflags.enter_cycle = move_spline.isCyclic();
 
 #ifdef LICH_KING

src/server/game/Movement/Spline/Spline.h
@@ -58,10 +58,10 @@ protected:
     typedef float (SplineBase::*SegLenghtMethtod)(index_type) const;
     static SegLenghtMethtod seglengths[ModesEnd];
 
-    void InitLinear(const Vector3*, index_type, bool, index_type);
-    void InitCatmullRom(const Vector3*, index_type, bool, index_type);
-    void InitBezier3(const Vector3*, index_type, bool, index_type);
-    typedef void (SplineBase::*InitMethtod)(const Vector3*, index_type, bool, index_type);
+    void InitLinear(const Vector3*, index_type, bool, index_type, float);
+    void InitCatmullRom(const Vector3*, index_type, bool, index_type, float);
+    void InitBezier3(const Vector3*, index_type, bool, index_type, float);
+    typedef void (SplineBase::*InitMethtod)(const Vector3*, index_type, bool, index_type, float);
     static InitMethtod initializers[ModesEnd];
 
     void UninitializedSpline() const { ABORT();}
@@ -95,8 +95,8 @@ public:
     const Vector3& getPoint(index_type i) const { return points[i];}
 
     /** Initializes spline. Don't call other methods while spline not initialized. */
-    void init_spline(const Vector3 * controls, index_type count, EvaluationMode m);
-    void init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point);
+    void init_spline(const Vector3 * controls, index_type count, EvaluationMode m, float initialOrientation);
+    void init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point, float initialOrientation);
 
     /** As i can see there are a lot of ways how spline can be initialized
         would be no harm to have some custom initializers. */
@@ -151,8 +151,8 @@ public:
     void computeIndex(float t, index_type& out_idx, float& out_u) const;
 
     /** Initializes spline. Don't call other methods while spline not initialized. */
-    void init_spline(const Vector3 * controls, index_type count, EvaluationMode m) { SplineBase::init_spline(controls, count, m);}
-    void init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point) { SplineBase::init_cyclic_spline(controls, count, m, cyclic_point);}
+    void init_spline(const Vector3 * controls, index_type count, EvaluationMode m, float initialOrientation) { SplineBase::init_spline(controls, count, m, initialOrientation);}
+    void init_cyclic_spline(const Vector3 * controls, index_type count, EvaluationMode m, index_type cyclic_point, float initialOrientation) { SplineBase::init_cyclic_spline(controls, count, m, cyclic_point, initialOrientation);}
 
     /**  Initializes lengths with SplineBase::SegLength method. */
     void initLengths();

src/server/game/Spells/SpellMgr.h
@@ -376,7 +376,7 @@ enum ProcFlags
 {
     PROC_FLAG_NONE                            = 0x00000000,
 
-    PROC_FLAG_KILLED                          = 0x00000001,    // 00 Killed by agressor - not sure about this flag
+    PROC_FLAG_KILLED                          = 0x00000001,    // 00 Killed by agressor - not sure about this flag (maybe PROC_FLAG_HEARTBEAT)
     PROC_FLAG_KILL                            = 0x00000002,    // 01 Kill target (in most cases need XP/Honor reward)
 
     PROC_FLAG_DONE_MELEE_AUTO_ATTACK          = 0x00000004,    // 02 Done melee auto attack

commit 264b928c7151d75a7b7b83f9cdfef18d662fdea9

Fixed spline movement broken yesterday in c3fcd8aa29613fec428752ebe6c3351113ebe496

src/server/game/Movement/Spline/Spline.cpp
@@ -261,6 +261,7 @@ void SplineBase::InitCatmullRom(const Vector3* controls, index_type count, bool 
     else
     {
         points[0] = controls[0] - G3D::Vector3{ cos(initialOrientation), sin(initialOrientation), 0.0f }; // sun: from TrinityCore/TrinityCore/issues/22435
+        points[high_index + 1] = controls[count - 1];
     }
 
     index_lo = lo_index;