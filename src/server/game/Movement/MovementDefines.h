f07bb5f48dc1474062f85f6000893758ce96af07
Kelno <kelnowm@gmail.com>
2018-09-26 12:58:59 +0200

    Missing part in last commit

--- src/server/game/Movement/MovementDefines.h
+++ src/server/game/Movement/MovementDefines.h
@@ -29,24 +29,26 @@
 // values 0 ... MAX_DB_MOTION_TYPE-1 used in DB
 enum MovementGeneratorType : uint8
 {
-    IDLE_MOTION_TYPE          = 0,                              // IdleMovementGenerator.h
-    RANDOM_MOTION_TYPE        = 1,                              // RandomMovementGenerator.h
-    WAYPOINT_MOTION_TYPE      = 2,                              // WaypointMovementGenerator.h
-    MAX_DB_MOTION_TYPE        = 3,                              // *** this and below motion types can't be set in DB.
-    ANIMAL_RANDOM_MOTION_TYPE = 3,                              // AnimalRandomMovementGenerator.h
-    CONFUSED_MOTION_TYPE      = 4,                              // ConfusedMovementGenerator.h
-    CHASE_MOTION_TYPE         = 5,                              // ChaseMovementGenerator.h
-    HOME_MOTION_TYPE          = 6,                              // HomeMovementGenerator.h
-    FLIGHT_MOTION_TYPE        = 7,                              // WaypointMovementGenerator.h
-    POINT_MOTION_TYPE         = 8,                              // PointMovementGenerator.h
-    FLEEING_MOTION_TYPE       = 9,                              // FleeingMovementGenerator.h
-    DISTRACT_MOTION_TYPE      = 10,                             // IdleMovementGenerator.h
-    ASSISTANCE_MOTION_TYPE    = 11,                             // PointMovementGenerator.h (first part of flee for assistance)
-    ASSISTANCE_DISTRACT_MOTION_TYPE = 12,                       // IdleMovementGenerator.h (second part of flee for assistance)
-    TIMED_FLEEING_MOTION_TYPE = 13,                             // FleeingMovementGenerator.h (alt.second part of flee for assistance)
-    FOLLOW_MOTION_TYPE        = 14,                             // FollowMovementGenerator.h
-    ROTATE_MOTION_TYPE        = 15,                             // IdleMovementGenerator.h
-    EFFECT_MOTION_TYPE        = 16,
+    IDLE_MOTION_TYPE                = 0,     // IdleMovementGenerator.h
+    RANDOM_MOTION_TYPE              = 1,     // RandomMovementGenerator.h
+    WAYPOINT_MOTION_TYPE            = 2,     // WaypointMovementGenerator.h
+    MAX_DB_MOTION_TYPE              = 3,     // *** this and below motion types can't be set in DB.
+    ANIMAL_RANDOM_MOTION_TYPE       = 3,     // AnimalRandomMovementGenerator.h
+    CONFUSED_MOTION_TYPE            = 4,     // ConfusedMovementGenerator.h
+    CHASE_MOTION_TYPE               = 5,     // ChaseMovementGenerator.h
+    HOME_MOTION_TYPE                = 6,     // HomeMovementGenerator.h
+    FLIGHT_MOTION_TYPE              = 7,     // WaypointMovementGenerator.h
+    POINT_MOTION_TYPE               = 8,     // PointMovementGenerator.h
+    FLEEING_MOTION_TYPE             = 9,     // FleeingMovementGenerator.h
+    DISTRACT_MOTION_TYPE            = 10,    // IdleMovementGenerator.h
+    ASSISTANCE_MOTION_TYPE          = 11,    // PointMovementGenerator.h
+    ASSISTANCE_DISTRACT_MOTION_TYPE = 12,    // IdleMovementGenerator.h 
+    TIMED_FLEEING_MOTION_TYPE       = 13,    // FleeingMovementGenerator.h 
+    FOLLOW_MOTION_TYPE              = 14,    // FollowMovementGenerator.h
+    ROTATE_MOTION_TYPE              = 15,    // IdleMovementGenerator.h
+    EFFECT_MOTION_TYPE              = 16,
+    //TC SPLINE_CHAIN_MOTION_TYPE        = 17,    // SplineChainMovementGenerator.h
+    FORMATION_MOTION_TYPE           = 18,    // FormationMovementGenerator.h
 
     MAX_MOTION_TYPE                          // limit
 };
