7bbcc16dbf823d3d69fea9daf9d37d1aab623fca
Kelno <kelnowm@gmail.com>
2018-09-29 17:39:16 +0200

    Fixed crashes in some .mod speed commands

---
+++ sql/updates/world/world_2018_09_29_01.sql
@@ -0,0 +1,4 @@
+UPDATE trinity_string SET content_default = "You set the speed of %s from normal to %2.2f.", content_loc2 = NULL WHERE entry = 139;
+UPDATE trinity_string SET content_default = "You set the swim speed of %s from normal to %2.2f.", content_loc2 = NULL WHERE entry = 141;
+UPDATE trinity_string SET content_default = "You set the backwards run speed of %s from normal to %2.2f.", content_loc2 = NULL WHERE entry = 143;
+UPDATE trinity_string SET content_default = "You set the fly speed of %s from normal to %2.2f.", content_loc2 = NULL WHERE entry = 145;
--- src/server/game/Movement/PathGenerator.cpp
+++ src/server/game/Movement/PathGenerator.cpp
@@ -110,7 +110,7 @@ bool PathGenerator::CalculatePath(float destX, float destY, float destZ, bool fo
     if (_sourceUnit && !_forceSourcePos)
     {
         _sourcePos.Relocate(_sourceUnit->GetPosition());
-        if(_transport) //ok if owner is in world, but not if he's on yet another transport... we'll that shouldn't happen
+        if(_transport) //ok if owner is in world, but not if he's on yet another transport... well that shouldn't happen
             _transport->CalculatePassengerOffset(_sourcePos.m_positionX, _sourcePos.m_positionY, _sourcePos.m_positionZ);
 
         // Always force destination if target is on transport and we're not, or we are on a transport and target is not
