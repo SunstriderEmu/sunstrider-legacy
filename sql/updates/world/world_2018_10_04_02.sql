d446b3077e08155b26754bcea7aadb79b383411b
Kelno <kelnowm@gmail.com>
2018-10-05 00:05:22 +0200

    Added some missing objects in arathi

---
+++ sql/updates/world/world_2018_10_04_02.sql
@@ -0,0 +1,8 @@
+replace into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`) 
+values ('166407','1744','529','1','991.053','1001.65','-42.6028','4.10843','0','0','0.885412','-0.464807','25','0','1','');
+replace into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`) 
+values ('166408','1744','529','1','991.352','1014.72','-42.604','1.22993','0','0','0.576931','0.816793','25','0','1','');
+replace into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`) 
+values ('166409','1744','529','1','996.232','1001.88','-42.6043','6.17009','0','0','0.0565189','-0.998402','25','0','1','');
+replace into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`) 
+values ('166410','1744','529','1','984.525','1007.21','-42.6043','2.23917','0','0','0.899921','0.436054','25','0','1','');
