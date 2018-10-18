SunstriderEmu/sunstrider-legacy

commit b121590fe130844b9821420750359156c6b91737

Fixed build errors on unix #2

src/server/game/Entities/Object/Object.cpp
@@ -2826,55 +2826,14 @@ void WorldObject::SetTransport(Transport* transport)
     m_transport = transport;
 }
 
-MovementInfo* WorldObject::_GetMovementInfo() const
+MovementInfo const& WorldObject::_GetMovementInfo()
 {
-    return &m_movementInfo;
+    return m_movementInfo;
 }
 
 MovementInfo WorldObject::GetMovementInfo() const
 {
-    MovementInfo mInfo;
-    mInfo.guid = GetGUID();
-    mInfo.SetMovementFlags(GetUnitMovementFlags());
-    mInfo.SetExtraMovementFlags(GetExtraUnitMovementFlags());
-    mInfo.time = m_movementInfo.time;
-    mInfo.pos.Relocate(
-        GetPositionX(),
-        GetPositionY(),
-        GetPositionZ(),
-        GetOrientation()
-    );
-
-    if (GetUnitMovementFlags() & MOVEMENTFLAG_ONTRANSPORT)
-    {
-        mInfo.transport.guid = GetTransGUID();
-        mInfo.transport.pos.Relocate(GetTransOffsetX(), GetTransOffsetY(), GetTransOffsetZ(), GetTransOffsetO());
-        mInfo.transport.time = GetTransTime();
-#ifdef LICH_KING
-        mInfo.transport.seat = GetTransSeat();
-        if (GetExtraUnitMovementFlags() & MOVEMENTFLAG2_INTERPOLATED_MOVEMENT)
-            mInfo.transport.time2 = m_movementInfo.transport.time2;
-#endif
-    }
-    if ((GetUnitMovementFlags() & (MOVEMENTFLAG_SWIMMING | MOVEMENTFLAG_JUMPING_OR_FALLING))
-#ifdef LICH_KING
-        || (m_movementInfo.flags2 & MOVEMENTFLAG2_ALWAYS_ALLOW_PITCHING)
-#endif
-        )
-        mInfo.pitch = m_movementInfo.pitch;
-
-    mInfo.SetFallTime(m_movementInfo.fallTime);
-    if (GetUnitMovementFlags() & MOVEMENTFLAG_JUMPING_OR_FALLING)
-    {
-        mInfo.jump.zspeed = m_movementInfo.jump.zspeed;
-        mInfo.jump.sinAngle = m_movementInfo.jump.sinAngle;
-        mInfo.jump.cosAngle = m_movementInfo.jump.cosAngle;
-        mInfo.jump.xyspeed = m_movementInfo.jump.xyspeed;
-    }
-    if (GetUnitMovementFlags() & MOVEMENTFLAG_SPLINE_ELEVATION)
-        mInfo.splineElevation = m_movementInfo.splineElevation;
-
-    return mInfo;
+    return m_movementInfo;
 }
 
 ObjectGuid WorldObject::GetCharmerOrOwnerOrOwnGUID() const

src/server/game/Entities/Object/Object.h
@@ -685,7 +685,7 @@ class TC_GAME_API WorldObject : public Object, public WorldLocation
 
         // Return the current positional/physical state of the object
         MovementInfo GetMovementInfo() const;
-        MovementInfo* _GetMovementInfo() const;
+        MovementInfo const& _GetMovementInfo();
         virtual float ComputeCollisionHeight() const { return 0.0f; }
         virtual float GetCollisionHeight() const { return 0.0f; }
         float GetMapWaterOrGroundLevel(float x, float y, float z, float* ground = nullptr) const;

src/server/game/Entities/Player/Player.cpp
@@ -23302,7 +23302,7 @@ bool Player::IsTestingBot() const
 #ifdef TESTS
     return GetTestingPlayerbotAI();
 #else
-    return nullptr;
+    return false;
 #endif
 }
 

src/server/game/Movement/MovementHandler.cpp
@@ -250,7 +250,8 @@ void WorldSession::HandleMoveTeleportAck(WorldPacket& recvData)
     }
 
     PlayerMovementPendingChange pendingChange = PopPendingMovementChange().second;
-    pendingChange.Resolve(pendingChange, this, mover, mover->_GetMovementInfo()); //GetMovementInfo wont be used here
+    MovementInfo info = mover->GetMovementInfo();
+    pendingChange.Resolve(pendingChange, this, mover, &info); //GetMovementInfo wont be used here
 }
 
 /*
@@ -1128,7 +1129,7 @@ void WorldSession::ResolveAllPendingChanges()
     while (HasPendingMovementChange())
     {
         PlayerMovementPendingChange pendingChange = PopPendingMovementChange().second;
-        pendingChange.Resolve(pendingChange, this, nullptr);
+        pendingChange.Resolve(pendingChange, this, nullptr, nullptr);
     }
 }
 
@@ -1193,6 +1194,7 @@ void PlayerMovementPendingChange::Resolve(PlayerMovementPendingChange const& cha
 {
     //if we're doing a serverside resolve, we're using the current movement info from the server, so no need to validate it
     bool serverSideResolve = mover == nullptr;
+    MovementInfo serverMovementInfo = mover->GetMovementInfo();
     if (serverSideResolve)
     {
         ASSERT(movementInfo == nullptr); //else, wrong usage of this function
@@ -1203,7 +1205,7 @@ void PlayerMovementPendingChange::Resolve(PlayerMovementPendingChange const& cha
                 guid.ToString().c_str(), session->GetPlayer()->GetName().c_str(), session->GetPlayer()->GetGUID().ToString().c_str());
             return;
         }
-        movementInfo = mover->_GetMovementInfo();
+        movementInfo = &serverMovementInfo;
     }
     else 
     {

commit 0cde5abc6c785e2def41548fdf67f7cd5a456835

Fixed teleport to new map (broken in b121590fe130844b9821420750359156c6b91737 )

src/server/game/Entities/Object/Object.cpp
@@ -160,8 +160,8 @@ void Object::BuildCreateUpdateBlockForPlayer(UpdateData *data, Player *target) c
     if(!target)
         return;
 
-    uint8  updateType = UPDATETYPE_CREATE_OBJECT;
-    uint8  flags = m_updateFlag;
+    uint8 updateType = UPDATETYPE_CREATE_OBJECT;
+    uint8 flags = m_updateFlag;
 
     // lower flag1
     if(target == this)                                      // building packet for oneself
@@ -2827,14 +2827,56 @@ void WorldObject::SetTransport(Transport* transport)
     m_transport = transport;
 }
 
-MovementInfo const& WorldObject::_GetMovementInfo()
+MovementInfo const& WorldObject::_GetMovementInfo() const
 {
     return m_movementInfo;
 }
 
 MovementInfo WorldObject::GetMovementInfo() const
 {
-    return m_movementInfo;
+    //almost a copy... except for the positions
+    MovementInfo mInfo;
+    mInfo.guid = GetGUID();
+    mInfo.SetMovementFlags(GetUnitMovementFlags());
+    mInfo.SetExtraMovementFlags(GetExtraUnitMovementFlags());
+    mInfo.time = m_movementInfo.time;
+    mInfo.pos.Relocate(
+        GetPositionX(),
+        GetPositionY(),
+        GetPositionZ(),
+        GetOrientation()
+    );
+
+    if (GetUnitMovementFlags() & MOVEMENTFLAG_ONTRANSPORT)
+    {
+        mInfo.transport.guid = GetTransGUID();
+        mInfo.transport.pos.Relocate(GetTransOffsetX(), GetTransOffsetY(), GetTransOffsetZ(), GetTransOffsetO());
+        mInfo.transport.time = GetTransTime();
+#ifdef LICH_KING
+        mInfo.transport.seat = GetTransSeat();
+        if (GetExtraUnitMovementFlags() & MOVEMENTFLAG2_INTERPOLATED_MOVEMENT)
+            mInfo.transport.time2 = m_movementInfo.transport.time2;
+#endif
+    }
+    if ((GetUnitMovementFlags() & (MOVEMENTFLAG_SWIMMING | MOVEMENTFLAG_JUMPING_OR_FALLING))
+#ifdef LICH_KING
+        || (m_movementInfo.flags2 & MOVEMENTFLAG2_ALWAYS_ALLOW_PITCHING)
+#endif
+        )
+        mInfo.pitch = m_movementInfo.pitch;
+
+    mInfo.SetFallTime(m_movementInfo.fallTime);
+    if (GetUnitMovementFlags() & MOVEMENTFLAG_JUMPING_OR_FALLING)
+    {
+        mInfo.jump.zspeed = m_movementInfo.jump.zspeed;
+        mInfo.jump.sinAngle = m_movementInfo.jump.sinAngle;
+        mInfo.jump.cosAngle = m_movementInfo.jump.cosAngle;
+        mInfo.jump.xyspeed = m_movementInfo.jump.xyspeed;
+    }
+    if (GetUnitMovementFlags() & MOVEMENTFLAG_SPLINE_ELEVATION)
+        mInfo.splineElevation = m_movementInfo.splineElevation;
+
+    return mInfo;
 }
 
 ObjectGuid WorldObject::GetCharmerOrOwnerOrOwnGUID() const

src/server/game/Entities/Object/Object.h
@@ -685,7 +685,7 @@ class TC_GAME_API WorldObject : public Object, public WorldLocation
 
         // Return the current positional/physical state of the object
         MovementInfo GetMovementInfo() const;
-        MovementInfo const& _GetMovementInfo();
+        MovementInfo const& _GetMovementInfo() const;
         virtual float ComputeCollisionHeight() const { return 0.0f; }
         virtual float GetCollisionHeight() const { return 0.0f; }
         float GetMapWaterOrGroundLevel(float x, float y, float z, float* ground = nullptr) const;

src/server/game/Entities/Player/Player.cpp
@@ -3889,7 +3889,7 @@ Mail* Player::GetMail(uint32 id)
     return nullptr;
 }
 
-void Player::BuildCreateUpdateBlockForPlayer( UpdateData *data, Player *target ) const
+void Player::BuildCreateUpdateBlockForPlayer(UpdateData *data, Player *target) const
 {
     for(uint8 i = 0; i < EQUIPMENT_SLOT_END; i++)
     {
@@ -3918,7 +3918,7 @@ void Player::BuildCreateUpdateBlockForPlayer( UpdateData *data, Player *target )
         }
     }
 
-    Unit::BuildCreateUpdateBlockForPlayer( data, target );
+    Unit::BuildCreateUpdateBlockForPlayer(data, target);
 }
 
 void Player::DestroyForPlayer(Player *target, bool onDeath) const
@@ -4359,6 +4359,7 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
     }
 
     // speed change, land walk
+    SetRooted(false);
 
     // remove death flag + set aura
     SetByteValue(UNIT_FIELD_BYTES_1, UNIT_BYTES_1_OFFSET_ANIM_TIER, 0x00);

src/server/game/Maps/Map.cpp
@@ -2314,7 +2314,7 @@ const char* Map::GetMapName() const
     return i_mapEntry ? i_mapEntry->name[sWorld->GetDefaultDbcLocale()] : "UNNAMEDMAP\x0";
 }
 
-void Map::SendInitSelf( Player * player)
+void Map::SendInitSelf(Player * player)
 {
     TC_LOG_DEBUG("maps","Creating player data for himself %u", player->GetGUID().GetCounter());
 

src/server/game/Movement/MovementHandler.cpp
@@ -83,7 +83,7 @@ void WorldSession::HandleMoveWorldportAck()
     player->ResetMap();
     player->SetMap(newMap);
 
-    // check this before Map::Add(player), because that will create the instance save!
+    // check this before Map::AddPlayerToMap(player), because that will create the instance save!
     bool reset_notify = (player->GetBoundInstance(player->GetMapId(), player->GetDifficulty()) == NULL);
 
     player->SendInitialPacketsBeforeAddToMap();