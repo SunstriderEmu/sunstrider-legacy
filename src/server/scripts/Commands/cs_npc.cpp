a765bb7485178cd568cb45f469901fbf6f67e14e
Kittnz <Kittnz@users.noreply.github.com>
2018-12-20 23:31:29 +0100

    Core/Command: Add Equipement Id & Armor to npc info (#92)

--- src/server/scripts/Commands/cs_npc.cpp
+++ src/server/scripts/Commands/cs_npc.cpp
@@ -835,10 +835,12 @@ public:
             handler->PSendSysMessage("Current id (heroic id ?) : %u", cInfo->Entry);
 
         handler->PSendSysMessage(LANG_NPCINFO_LEVEL, target->GetLevel());
+        handler->PSendSysMessage("EquipmentId: %u.", target->GetCurrentEquipmentId());
         handler->PSendSysMessage(LANG_NPCINFO_HEALTH,target->GetCreateHealth(), target->GetMaxHealth(), target->GetHealth());
         handler->PSendSysMessage(LANG_NPCINFO_FLAGS, target->GetUInt32Value(UNIT_FIELD_FLAGS), target->GetUInt32Value(UNIT_DYNAMIC_FLAGS), target->GetFaction());
         handler->PSendSysMessage(LANG_COMMAND_RAWPAWNTIMES, defRespawnDelayStr.c_str(),curRespawnDelayStr.c_str());
         handler->PSendSysMessage(LANG_NPCINFO_LOOT,  cInfo->lootid,cInfo->pickpocketLootId,cInfo->SkinLootId);
+        handler->PSendSysMessage("Armor: %u", target->GetArmor());
         handler->PSendSysMessage(LANG_NPCINFO_DUNGEON_ID, target->GetInstanceId());
         handler->PSendSysMessage(LANG_NPCINFO_POSITION,float(target->GetPositionX()), float(target->GetPositionY()), float(target->GetPositionZ()), float(target->GetOrientation()));
         handler->PSendSysMessage("AIName: %s", target->GetAIName().c_str());
