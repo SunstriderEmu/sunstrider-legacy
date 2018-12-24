0c68991b757b5dfde9cda4e1bdbdcd74f633aafc
Kelno <kelnowm@gmail.com>
2018-12-24 15:22:00 +0100

    Added missing spell ranks for deep wound proc

--- sql/updates/world/world_2018_12_24_01.sql
+++ sql/updates/world/world_2018_12_24_01.sql
@@ -1,2 +1,6 @@
 REPLACE INTO spell_script_names VALUES (-12834, "spell_warr_deep_wounds_aura");
 REPLACE INTO spell_script_names VALUES (-12162, "spell_warr_deep_wounds");
+
+replace into `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) values('12162','12162','1');
+replace into `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) values('12162','12850','2');
+replace into `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) values('12162','12868','3');
