3b28e1867501df61aab4770e0f5a55cfcc78e9e8
Kelno <kelnowm@gmail.com>
2018-12-28 19:54:42 +0100

    Added Orgrimmar roof stalkers

---
+++ sql/updates/world/world_2018_12_28_05.sql
@@ -0,0 +1,55 @@
+-- Importing creature with guid 40491 with import type REPLACE_ALL
+INSERT IGNORE INTO creature_entry (spawnID, entry) VALUES (40491, 23090);
+INSERT IGNORE INTO creature (spawnID, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, unit_flags, patch_min, patch_max) VALUES (40491, 1, 1, 0, 1, 1651.35, -4448.69,  38.0034, 1.59431, 300, 0, 0, 104790, 0, 2, 0, 0, 10);
+INSERT IGNORE INTO waypoint_data VALUES 
+(404910, 1, 1651.35, -4448.69, 38.0034, 0, 0, 0, NULL, 100, 0),
+(404910, 2, 1651.75, -4437.17, 38.0053, 0, 0, 0, NULL, 100, 0),
+(404910, 3, 1648.66, -4432.08, 37.8909, 0, 0, 0, NULL, 100, 0),
+(404910, 4, 1633.78, -4428.19, 38.146, 0, 0, 0, NULL, 100, 0),
+(404910, 5, 1628.51, -4430.07, 38.1318, 0, 0, 0, NULL, 100, 0),
+(404910, 6, 1620.25, -4441.33, 37.8515, 0, 0, 0, NULL, 100, 0),
+(404910, 7, 1620.04, -4447.66, 37.9152, 0, 0, 0, NULL, 100, 0),
+(404910, 8, 1628.39, -4458.96, 38.0537, 0, 0, 0, NULL, 100, 0),
+(404910, 9, 1620.04, -4447.66, 37.9152, 0, 0, 0, NULL, 100, 0),
+(404910, 10, 1620.25, -4441.33, 37.8515, 0, 0, 0, NULL, 100, 0),
+(404910, 11, 1628.51, -4430.07, 38.1318, 0, 0, 0, NULL, 100, 0),
+(404910, 12, 1633.78, -4428.19, 38.146, 0, 0, 0, NULL, 100, 0),
+(404910, 13, 1648.66, -4432.08, 37.8909, 0, 0, 0, NULL, 100, 0),
+(404910, 14, 1651.75, -4437.17, 38.0053, 0, 0, 0, NULL, 100, 0),
+(404910, 15, 1651.35, -4448.69, 38.0034, 0, 0, 0, NULL, 100, 0);
+INSERT IGNORE INTO creature_addon (spawnID, path_id, mount, bytes0, bytes1, bytes2, emote, auras) VALUES (40491, 404910, 0, 0, 0, 1, 0, "18950 32199");
+INSERT IGNORE INTO creature_entry (spawnID, entry) VALUES (40490, 23090);
+INSERT IGNORE INTO creature (spawnID, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, unit_flags, patch_min, patch_max) VALUES (40490, 1, 1, 0, 1, 1685.87, -4437.94,  36.0057, 2.50928, 300, 0, 0, 104790, 0, 2, 0, 0, 10);
+INSERT IGNORE INTO waypoint_data VALUES 
+(404900, 1, 1685.87, -4437.94, 36.0057, 0, 0, 0, NULL, 100, 0),
+(404900, 2, 1681.13, -4434.01, 35.3238, 0, 0, 0, NULL, 100, 0),
+(404900, 3, 1673.04, -4431.93, 34.8667, 0, 0, 0, NULL, 100, 0),
+(404900, 4, 1665.67, -4435.66, 34.8775, 0, 0, 0, NULL, 100, 0),
+(404900, 5, 1661.73, -4441.26, 34.9755, 0, 0, 0, NULL, 100, 0),
+(404900, 6, 1661.59, -4447.54, 35.0587, 0, 0, 0, NULL, 100, 0),
+(404900, 7, 1661.73, -4441.26, 34.9755, 0, 0, 0, NULL, 100, 0),
+(404900, 8, 1665.67, -4435.66, 34.8775, 0, 0, 0, NULL, 100, 0),
+(404900, 9, 1673.04, -4431.93, 34.8667, 0, 0, 0, NULL, 100, 0),
+(404900, 10, 1681.13, -4434.01, 35.3238, 0, 0, 0, NULL, 100, 0),
+(404900, 11, 1685.87, -4437.94, 36.0057, 0, 0, 0, NULL, 100, 0);
+INSERT IGNORE INTO creature_addon (spawnID, path_id, mount, bytes0, bytes1, bytes2, emote, auras) VALUES (40490, 404900, 0, 0, 0, 1, 0, "18950 32199");
+INSERT IGNORE INTO creature_entry (spawnID, entry) VALUES (40489, 23090);
+INSERT IGNORE INTO creature (spawnID, map, spawnMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, unit_flags, patch_min, patch_max) VALUES (40489, 1, 1, 0, 1, 1708.89, -4407.32,  40.1085, 2.35614, 300, 0, 0, 104790, 0, 2, 0, 0, 10);
+INSERT IGNORE INTO waypoint_data VALUES 
+(404890, 1, 1708.89, -4407.32, 40.1085, 0, 0, 0, NULL, 100, 0),
+(404890, 2, 1699.21, -4398.37, 40.5289, 0, 0, 0, NULL, 100, 0),
+(404890, 3, 1694.41, -4397.78, 40.6138, 0, 0, 0, NULL, 100, 0),
+(404890, 4, 1682.94, -4404.49, 40.6853, 0, 0, 0, NULL, 100, 0),
+(404890, 5, 1680.72, -4409.19, 40.8754, 0, 0, 0, NULL, 100, 0),
+(404890, 6, 1682.71, -4422.11, 40.4622, 0, 0, 0, NULL, 100, 0),
+(404890, 7, 1686.17, -4425.78, 40.6093, 0, 0, 0, NULL, 100, 0),
+(404890, 8, 1698.87, -4427.43, 40.7538, 0, 0, 0, NULL, 100, 0),
+(404890, 9, 1686.17, -4425.78, 40.6093, 0, 0, 0, NULL, 100, 0),
+(404890, 10, 1682.71, -4422.11, 40.4622, 0, 0, 0, NULL, 100, 0),
+(404890, 11, 1680.72, -4409.19, 40.8754, 0, 0, 0, NULL, 100, 0),
+(404890, 12, 1682.94, -4404.49, 40.6853, 0, 0, 0, NULL, 100, 0),
+(404890, 13, 1694.41, -4397.78, 40.6138, 0, 0, 0, NULL, 100, 0),
+(404890, 14, 1699.21, -4398.37, 40.5289, 0, 0, 0, NULL, 100, 0),
+(404890, 15, 1708.89, -4407.32, 40.1085, 0, 0, 0, NULL, 100, 0);
+INSERT IGNORE INTO creature_addon (spawnID, path_id, mount, bytes0, bytes1, bytes2, emote, auras) VALUES (40489, 404890, 0, 0, 0, 1, 0, "18950 32199");
+
