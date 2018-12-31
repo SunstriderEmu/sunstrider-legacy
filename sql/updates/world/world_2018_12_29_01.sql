561b63bc96b4cede17f29387137407807fc63cad
Kelno <kelnowm@gmail.com>
2018-12-31 16:44:58 +0100

    Import a lof more missing creatures/waypoints/formations from TC

---
+++ sql/updates/world/world_2018_12_29_01.sql
@@ -0,0 +1,3 @@
+
+insert ignore into `spawn_group_template` (`groupId`, `groupName`, `groupFlags`) values('31','Onyxia\'s Lair - Onyxia','4');
+DELETE FROM smart_scripts WHERE entryorguid IN (-1056990, -1069204, -1073600);
