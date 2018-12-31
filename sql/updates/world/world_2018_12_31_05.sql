8864153355570fecc801cd5e731a12222eca6e9d
Kelno <kelnowm@gmail.com>
2018-12-31 18:21:29 +0100

    Added two missing creature mounts

---
+++ sql/updates/world/world_2018_12_31_05.sql
@@ -0,0 +1,3 @@
+
+insert into `creature_template_addon` (`entry`, `mount`) values('24497', '22631') ON DUPLICATE KEY UPDATE mount = '22631';
+insert into `creature_template_addon` (`entry`, `mount`) values('25063', '28417') ON DUPLICATE KEY UPDATE mount = '28417';
