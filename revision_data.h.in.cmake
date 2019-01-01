d3938e52f82df3f6ac94026d5381e0b9c8b4dd73
Kelno <kelnowm@gmail.com>
2019-01-01 04:45:54 +0100

    New release version

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -9,9 +9,9 @@
  #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "world_2018_12_22.sql"
+ #define _FULL_DATABASE             "world_2019_01_01.sql"
  #define VER_COMPANYNAME_STR        "Sunstrider Core"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2018 Sunstrider"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2019 Sunstrider"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
