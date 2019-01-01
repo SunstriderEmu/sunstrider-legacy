257124c9bb73dc1c6a4e6421b0b91d66caaebfb6
Kelno <kelnowm@gmail.com>
2019-01-01 04:29:20 +0100

    Fixed some more warnings related to gossips

---
+++ sql/updates/world/world_2019_01_01_03.sql
@@ -0,0 +1,21 @@
+UPDATE broadcast_text SET MaleText = REPLACE(MaleText,'$N','$n');
+UPDATE broadcast_text SET MaleText = REPLACE(MaleText,'$N','$n');
+UPDATE broadcast_text SET FemaleText = REPLACE(FemaleText,'$N','$n');
+UPDATE broadcast_text SET FemaleText = REPLACE(FemaleText,'$N','$n');
+
+UPDATE gossip_text SET text0_0 = REPLACE(text0_0,'$N','$n');
+UPDATE gossip_text SET text0_1 = REPLACE(text0_1,'$N','$n');
+UPDATE gossip_text SET text1_0 = REPLACE(text1_0,'$N','$n');
+UPDATE gossip_text SET text1_1 = REPLACE(text1_1,'$N','$n');
+UPDATE gossip_text SET text2_0 = REPLACE(text2_0,'$N','$n');
+UPDATE gossip_text SET text2_1 = REPLACE(text2_1,'$N','$n');
+UPDATE gossip_text SET text3_0 = REPLACE(text3_0,'$N','$n');
+UPDATE gossip_text SET text3_1 = REPLACE(text3_1,'$N','$n');
+UPDATE gossip_text SET text4_0 = REPLACE(text4_0,'$N','$n');
+UPDATE gossip_text SET text4_1 = REPLACE(text4_1,'$N','$n');
+UPDATE gossip_text SET text5_0 = REPLACE(text5_0,'$N','$n');
+UPDATE gossip_text SET text5_1 = REPLACE(text5_1,'$N','$n');
+UPDATE gossip_text SET text6_0 = REPLACE(text6_0,'$N','$n');
+UPDATE gossip_text SET text6_1 = REPLACE(text6_1,'$N','$n');
+UPDATE gossip_text SET text7_0 = REPLACE(text7_0,'$N','$n');
+UPDATE gossip_text SET text7_1 = REPLACE(text7_1,'$N','$n');
