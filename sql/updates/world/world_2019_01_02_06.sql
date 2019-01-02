f6c81a19848db4cebacdbac7f6813db0e53a53d0
Kelno <kelnowm@gmail.com>
2019-01-02 15:36:03 +0100

    Fixed query

--- sql/updates/world/world_2019_01_02_06.sql
+++ sql/updates/world/world_2019_01_02_06.sql
@@ -1,2 +1,3 @@
 UPDATE gameobject_template set patch = 0 WHERE `name` LIKE "Dummy TLK %";
-UPDATE creature_template set patch = 0 WHERE `name` LIKE "Dummy TLK %";
+DELETE FROM creature_template WHERE entry IN (36296, 34564, 36530, 36885, 38208) AND patch = 5;
+UPDATE creature_template set patch = 0 WHERE `name` LIKE "Dummy TLK %";	
