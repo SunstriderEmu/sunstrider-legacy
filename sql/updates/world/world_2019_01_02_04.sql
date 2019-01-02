ff0a688bb8a17f24ce4cbae6a9d8989aa5421e28
Kelno <kelnowm@gmail.com>
2019-01-02 07:09:40 +0100

    Fixed two gob smart scripts

--- sql/updates/world/world_2019_01_02_04.sql
+++ sql/updates/world/world_2019_01_02_04.sql
@@ -10,3 +10,5 @@ ALTER TABLE `smart_scripts`
     ADD COLUMN IF NOT EXISTS `patch_max` TINYINT(2) UNSIGNED DEFAULT 10  NOT NULL AFTER patch_min;
 
 UPDATE smart_scripts ss LEFT JOIN spell_template st ON st.entry = ss.action_param1 SET ss.patch_min = 5 WHERE ss.action_type = 11 AND st.entry IS NULL;
+
+UPDATE smart_scripts SET event_param = 4 WHERE source_type = 1 AND event_type = 104 AND event_param = 8;
