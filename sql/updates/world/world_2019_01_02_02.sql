9a85fabff223cca7ff0893d0dcca00a5377e00cf
Kelno <kelnowm@gmail.com>
2019-01-02 02:09:06 +0100

    Added some missing areatrigger quest relations

---
+++ sql/updates/world/world_2019_01_02_02.sql
@@ -0,0 +1,10 @@
+
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('1667','1265');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('173','437');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('2207','5156');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('2726','6185');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('4064','9160');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('4280','9700');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('4581','10750');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('4588','10772');
+insert into `areatrigger_involvedrelation` (`id`, `quest`) values('3991','1658');
