265324bfd9dcbf3d7de4b43c6b3bf4d49581089d
Kelno <kelnowm@gmail.com>
2019-01-03 19:35:24 +0100

    Restore broken flags_extra db data in recent commits

--- sql/updates/world/world_2019_01_03_02.sql
+++ sql/updates/world/world_2019_01_03_02.sql
@@ -1,3 +1,3 @@
 DELETE FROM gameobject WHERE guid IN (1004707, 1005125, 1042392, 1042403, 1004947, 1042391, 1031505, 67870);
-UPDATE gameobject SET patch = 5 WHERE guid IN (61893, 203);
-UPDATE creature SET patch = 5 WHERE spawnID IN (125681, 125682);
+UPDATE gameobject SET patch_min = 5 WHERE guid IN (61893, 203);
+UPDATE creature SET patch_min = 5 WHERE spawnID IN (125681, 125682);
