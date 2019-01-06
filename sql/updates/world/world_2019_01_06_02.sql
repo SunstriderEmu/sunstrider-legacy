234a6bdc659e0ad2f83e864b935d2eb152e004ab
Kelno <kelnowm@gmail.com>
2019-01-06 22:20:20 +0100

    Update various smart scripts texts using $N / $C / $R but not targeting a player

---
+++ sql/updates/world/world_2019_01_06_02.sql
@@ -0,0 +1,15 @@
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 2433 AND source_type = 0 AND id = 1;
+UPDATE smart_scripts SET target_type = 7 WHERE entryorguid = 3448 AND source_type = 0 AND id = 3;
+UPDATE smart_scripts SET target_type = 7 WHERE entryorguid = 4049 AND source_type = 0 AND id = 0;
+UPDATE smart_scripts SET target_type = 21 WHERE entryorguid = 13716 AND source_type = 0 AND id = 16;
+UPDATE smart_scripts SET target_type = 7 WHERE entryorguid = 15371 AND source_type = 0;
+UPDATE smart_scripts SET target_type = 7 WHERE entryorguid = 20232 AND source_type = 0;
+
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 2015 AND source_type = 0 AND id = 0;
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 2428 AND source_type = 0 AND id = 2;
+
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 891  AND source_type = 0 AND id = 0;
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 5974 AND source_type = 0 AND id = 1;
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 5976 AND source_type = 0 AND id = 1;
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 5977 AND source_type = 0 AND id = 1;
+UPDATE smart_scripts SET target_type = 2 WHERE entryorguid = 5978 AND source_type = 0 AND id = 2;
