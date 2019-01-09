4c617e99bd72acfb208248a84936930a5932abd3
Kelno <kelnowm@gmail.com>
2019-01-09 23:19:20 +0100

    Fixed some smart scripts targeting by guid instead of entry

---
+++ sql/updates/world/world_2019_01_09_05.sql
@@ -0,0 +1,6 @@
+DELETE FROM smart_scripts WHERE entryorguid = -1310811;
+UPDATE smart_scripts SET target_type = 9, target_param1 = 19467, target_param2 = 0, target_param3 = 100 WHERE target_type = 10 AND target_param1 = 1310948;
+UPDATE smart_scripts SET target_type = 9, target_param1 = 19466, target_param2 = 0, target_param3 = 100 WHERE target_type = 10 AND target_param1 = 1310950;
+UPDATE smart_scripts SET target_type = 9, target_param1 = 20132, target_param2 = 0, target_param3 = 100 WHERE target_type = 10 AND target_param1 = 71631;
+UPDATE smart_scripts SET target_type = 9, target_param1 = 20929, target_param2 = 0, target_param3 = 100 WHERE target_type = 10 AND target_param1 = 1072857;
+UPDATE smart_scripts SET target_type = 9, target_param1 = 20929, target_param2 = 0, target_param3 = 100 WHERE target_type = 10 AND target_param1 = 1310951;
