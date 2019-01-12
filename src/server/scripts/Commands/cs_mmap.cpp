0ff419067266657ede20c2322d2ed882e9d0d590
Kelno <kelnowm@gmail.com>
2019-01-12 02:26:41 +0100

    Added .mmap pathfix command

--- src/server/scripts/Commands/cs_mmap.cpp
+++ src/server/scripts/Commands/cs_mmap.cpp
@@ -4,6 +4,10 @@
 #include "PathGenerator.h"
 #include "Transport.h"
 #include "MoveSplineInit.h"
+#include "WaypointManager.h"
+#include "WaypointMovementGenerator.h"
+#include "MapManager.h"
+#include <fstream>
 
 class mmaps_commandscript : public CommandScript
 {
@@ -19,7 +23,8 @@ public:
             { "loadedtiles",    SEC_GAMEMASTER3,     false, &HandleMmapLoadedTilesCommand,     "" },
             { "stats",          SEC_GAMEMASTER3,     false, &HandleMmapStatsCommand,           "" },
             { "testarea",       SEC_GAMEMASTER3,     false, &HandleMmapTestAreaCommand,        "" },
-            { "reload",         SEC_GAMEMASTER3,     false, &handleMmapReloadCommand,          "" },
+            { "reload",         SEC_GAMEMASTER3,     false, &HandleMmapReloadCommand,          "" },
+            { "fixpath",        SEC_SUPERADMIN,      true,  &HandleFixPathCommand,             "" },
         };
         static std::vector<ChatCommand> commandTable =
         {
@@ -286,7 +291,7 @@ public:
         return true;
     }
 
-    static bool handleMmapReloadCommand(ChatHandler* handler, char const* args)
+    static bool HandleMmapReloadCommand(ChatHandler* handler, char const* args)
     {
         Player* player = handler->GetSession()->GetPlayer();
 
@@ -295,6 +300,218 @@ public:
 
         player->GetMap()->ReloadMMap(gx, gy);
         handler->PSendSysMessage("Triggered reload from mmap at coord (%i,%i)", gx, gy);
+        return true;
+    }
+
+    static bool HandleFixPathCommand(ChatHandler* handler, char const* args)
+    { 
+        uint32 const DEBUG_PATH_ID = 0; // 694990; 
+        CreatureDataContainer const& allCreatures = sObjectMgr->GetAllCreatureData();
+        float const MAX_CHECK_DIST = 3.0f;
+        float const MAX_GROUND_DIST = MAX_CHECK_DIST / 2;
+        uint32 const DEBUG_LIMIT = 0;
+        uint32 modified = 0;
+
+        std::ofstream outfile("rework_path.txt", std::ofstream::out);
+        std::ofstream outfileSQL("rework_path.sql", std::ofstream::out);
+
+        for (auto itr : allCreatures)
+        {
+            if (DEBUG_LIMIT && modified >= DEBUG_LIMIT)
+                break;
+
+            uint32 spawnID = itr.first;
+            CreatureData const& creData = itr.second;
+            CreatureAddon const* addon = sObjectMgr->GetCreatureAddon(spawnID);
+            if (!addon)
+                continue;
+
+            uint32 pathID = addon->path_id;
+            if (!pathID)
+                continue;
+
+            if (DEBUG_PATH_ID && pathID != DEBUG_PATH_ID)
+                continue;
+
+            CreatureTemplate const* templatInfo = sObjectMgr->GetCreatureTemplate(creData.GetFirstSpawnEntry());
+            if (!templatInfo)
+            {
+                //MEH
+                ASSERT(false);
+                continue;
+            }
+
+            CreatureMovementData const& movementData = templatInfo->Movement;
+            if (movementData.IsFlightAllowed())
+                continue;
+
+            WaypointPath const* original_path = sWaypointMgr->GetPath(pathID);
+            if (!original_path)
+            {
+                //unknown path? What to do?
+                continue;
+            }
+            if (original_path->pathDirection == WP_PATH_DIRECTION_RANDOM)
+            {
+                //CANT HELP THAT LOL
+                continue;
+            }
+
+            Map const* map = sMapMgr->CreateBaseMap(creData.spawnPoint.GetMapId());
+            if (!map)
+                continue;
+
+            WaypointPath path;
+            bool lastPoint = false;
+            if (original_path->nodes.size() == 1)
+                continue;
+
+            for (auto itr = original_path->nodes.begin(); itr != original_path->nodes.end(); itr++)
+            {
+                WaypointNode newNode = *itr;
+                newNode.eventChance = newNode.id + 1; //Mark this point as non removable
+                path.nodes.push_back(newNode);
+                auto nextItr = std::next(itr);
+                if (nextItr == original_path->nodes.end())
+                {
+                    if (original_path->pathType == WP_PATH_TYPE_LOOP)
+                    {
+                        nextItr = original_path->nodes.begin();
+                        lastPoint = true;
+                    }
+                    else
+                        break;
+                }
+
+                G3D::Vector3 start = G3D::Vector3(itr->x, itr->y, itr->z);
+                G3D::Vector3 end = G3D::Vector3(nextItr->x, nextItr->y, nextItr->z);
+                G3D::Vector3 diff = end - start;
+
+                PathGenerator pathGenerator(Position(start.x, start.y, start.z), map->GetId());
+                pathGenerator.SetPathLengthLimit(diff.length() * 2);
+                bool success = pathGenerator.CalculatePath(end.x, end.y, end.z, true, false);
+                if (success)
+                    if (pathGenerator.GetPathType() & (PATHFIND_SHORT | PATHFIND_NOPATH | PATHFIND_INCOMPLETE | PATHFIND_NOT_USING_PATH))
+                        success = false;
+
+                if (success)
+                {
+                    Movement::PointsArray points = pathGenerator.GetPath();
+                    points.erase(points.begin());
+                    points.pop_back();
+                    for (auto point : points)
+                    {
+                        WaypointNode intermediateNode(0, point);
+                        intermediateNode.eventChance = 0;
+                        intermediateNode.moveType = newNode.moveType;
+                        path.nodes.push_back(intermediateNode);
+                    }
+                }
+
+                if (lastPoint)
+                    break;
+            }
+
+            //step 2: discard many many points
+            lastPoint = false;
+            WaypointNode lastNode = *(path.nodes.begin());
+            for (auto itr = ++path.nodes.begin(); itr != path.nodes.end();) //start at node 2
+            {
+                WaypointNode& currentNode = *itr;
+                if (currentNode.eventChance) //point is non removable
+                    goto prepare_next_loop;
+
+                {
+                    WaypointNode const& previousNode = lastNode;
+                    auto nextItr = std::next(itr);
+                    if (nextItr == path.nodes.end())
+                    {
+                        if (original_path->pathType == WP_PATH_TYPE_LOOP)
+                        {
+                            nextItr = path.nodes.begin();
+                            lastPoint = true;
+                        }
+                        else
+                            break;
+                    }
+
+                    WaypointNode const& nextNode = *nextItr;
+
+                    G3D::Vector3 start(previousNode.x, previousNode.y, previousNode.z);
+                    G3D::Vector3 delta = G3D::Vector3(nextNode.x, nextNode.y, nextNode.z) - start;
+                    G3D::Vector3 deltaCurrent = G3D::Vector3(currentNode.x, currentNode.y, currentNode.z) - start;
+                    G3D::Vector3 pointCurrent = G3D::Vector3(currentNode.x, currentNode.y, currentNode.z);
+
+                    //approximative
+                    G3D::Vector3 pointLine = start + delta * (deltaCurrent.length() / delta.length());
+                    if ((pointCurrent - pointLine).length() < MAX_GROUND_DIST)
+                    {
+                        itr = path.nodes.erase(itr);
+                        //then last node is still the same
+                        continue;
+                    }
+                }
+
+                if (lastPoint)
+                    break;
+
+                //prepare next loop
+            prepare_next_loop:
+                lastNode = currentNode;
+                itr++;
+            }
+
+            //step3: write ids
+            uint32 id = 1;
+            for (auto& itr : path.nodes)
+                itr.id = id++;
+
+            bool changed = path.nodes.size() > original_path->nodes.size();
+            bool warn = path.nodes.size() > original_path->nodes.size()*2;
+
+            //if path was changed, write new path
+            if (changed)
+            {
+                modified++;
+                outfileSQL << "DELETE FROM `waypoint_data` WHERE `id` = " << pathID << ";" << std::endl;
+                outfile << "PATH: " << pathID << ", SPAWN: " << spawnID;
+                if (warn)
+                    outfile << ", WARN!";
+
+                outfile << std::endl;
+                outfileSQL << "INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `comment`) VALUES " << std::endl;
+                auto itr2 = path.nodes.begin();
+                for (; itr2 != path.nodes.end(); itr2++)
+                {
+                    outfile << pathID << ',' << itr2->id << ',' << itr2->x << ',' << itr2->y << ',' << itr2->z << ',' << itr2->orientation << ',' << itr2->delay << ',' << itr2->moveType << ',' << itr2->eventId;
+                    if (itr2->eventChance)
+                        outfile << " (original id:" << uint32(itr2->eventChance-1) << ")";
+
+                    outfile << std::endl;
+
+                    outfileSQL << "(" << pathID << ", " << itr2->id << ", " << itr2->x << ", " << itr2->y << ", " << itr2->z << ", " << itr2->orientation << ", " << itr2->delay << ", " << itr2->moveType << ", ";
+                    if (itr2->eventId)
+                        outfileSQL << itr2->eventId;
+                    else
+                        outfileSQL << "NULL";
+
+                    outfileSQL << ", 'autogenerated with `.mmap fixpath`')";
+                    if (std::next(itr2) != path.nodes.end())
+                        outfileSQL << ",";
+                    else
+                        outfileSQL << ";";
+
+                    if (itr2->eventChance)
+                        outfileSQL << " -- original id: " << uint32(itr2->eventChance - 1);
+
+                    outfileSQL << std::endl;
+                }
+                outfileSQL << "UPDATE `creature_addon` SET `path_id` = '" << pathID << "' WHERE `spawnID` = " << spawnID << ";" << std::endl << std::endl;
+            }
+        }
+        outfile.close();
+        outfileSQL.close();
+        handler->PSendSysMessage("Finished %u", modified);
         return true;
     }
 };
