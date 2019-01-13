a389e2da3b83db49b61379cf4d66ae2d33cd9b0b
Kelno <kelnowm@gmail.com>
2019-01-13 08:23:44 +0100

    Added some missing spawns in Blade's Edge

---
+++ sql/updates/world/world_2019_01_13_01.sql
@@ -0,0 +1,226 @@
+-- UPDATE creature SET IMPORT = "REPLACE_ALL" WHERE id IN (22243, 22241)
+
+START TRANSACTION;
+-- Importing creature spawn with guid 77946 with import type REPLACE_ALL
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95109', '22241');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95109', '530', '1', NULL, '1', '4023.13', '5849.69', '266.066', '3.75246', '300', '0', '0', '6986', '0', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95110', '22241');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95110', '530', '1', NULL, '1', '4000.29', '5894.65', '266.54', '3.87463', '300', '0', '0', '7181', '0', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95150', '22241');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95150', '530', '1', NULL, '1', '3957.18', '6082.14', '269.598', '4.86947', '300', '0', '0', '7181', '0', '0', '0', '0', '0', '10');
+
+
+-- Importing creature spawn with guid 77947 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77948 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77949 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77950 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77951 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77952 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77953 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77954 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77955 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77956 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77969 with import type REPLACE_ALL
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95111', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95111', '530', '1', NULL, '1', '3912.05', '6093.59', '270.567', '5.58119', '300', '0', '0', '5903', '3309', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95149', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95149', '530', '1', NULL, '1', '3695.35', '6004.69', '265.265', '5.92225', '300', '0', '0', '5744', '3231', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95151', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95151', '530', '1', NULL, '1', '4006.68', '5924.28', '267.573', '4.53786', '300', '0', '0', '5744', '3231', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95152', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95152', '530', '1', NULL, '1', '4040.06', '5875.89', '267.873', '3.47321', '300', '0', '0', '5903', '3309', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95153', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95153', '530', '1', NULL, '1', '3981.69', '6067.01', '266.442', '3.64426', '300', '0', '0', '5903', '3309', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95154', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95154', '530', '1', NULL, '1', '3926.36', '6023.15', '269.678', '1.71042', '300', '0', '0', '5744', '3231', '0', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95167', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95167', '530', '1', NULL, '1', '4053.62', '5601.31', '267.276', '2.25469', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95168', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95168', '530', '1', NULL, '1', '4059.15', '5594.06', '267.276', '5.51384', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95169', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95169', '530', '1', NULL, '1', '4069.72', '5584.58', '267.276', '2.14057', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95170', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95170', '530', '1', NULL, '1', '4075.51', '5600.23', '267.275', '0.718997', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95171', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95171', '530', '1', NULL, '1', '4053.37', '5578.36', '267.275', '3.89201', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95195', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95195', '530', '1', NULL, '1', '4075.62', '5578.02', '267.276', '5.44317', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95196', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95196', '530', '1', NULL, '1', '3917.18', '5117.5', '270.091', '0.593332', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95197', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95197', '530', '1', NULL, '1', '3910.86', '5107.54', '270.091', '4.00589', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95202', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95202', '530', '1', NULL, '1', '3901.1', '5096.72', '270.093', '0.766121', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95203', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95203', '530', '1', NULL, '1', '3895.51', '5087.14', '270.093', '4.02945', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95204', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95204', '530', '1', NULL, '1', '3916.58', '5086.17', '270.093', '5.55705', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95205', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95205', '530', '1', NULL, '1', '3894.64', '5117.94', '270.093', '2.15628', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95207', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95207', '530', '1', NULL, '1', '4005.71', '5324.08', '269.73', '4.00589', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95208', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95208', '530', '1', NULL, '1', '4009.55', '5332.21', '269.73', '0.589409', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95211', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95211', '530', '1', NULL, '1', '4019.61', '5342.73', '269.731', '3.90379', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95212', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95212', '530', '1', NULL, '1', '4024.58', '5351.21', '269.731', '0.899641', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95237', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95237', '530', '1', NULL, '1', '4005.75', '5351.17', '269.732', '2.10523', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+INSERT INTO creature_entry (`spawnID`, `entry`) VALUES ('95239', '22243');
+INSERT INTO creature (`spawnID`, `map`, `spawnMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `unit_flags`, `pool_id`, `patch_min`, `patch_max`) VALUES ('95239', '530', '1', NULL, '1', '4023.99', '5322.84', '269.73', '5.50208', '300', '3', '0', '5903', '3309', '1', '0', '0', '0', '10');
+
+
+-- Importing creature spawn with guid 77970 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77971 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77972 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77973 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77974 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77975 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77976 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77977 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77978 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77979 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77980 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77981 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77982 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77983 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77984 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77985 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 77986 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95109 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95110 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95111 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95149 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95150 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95151 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95152 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95153 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95154 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95167 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95168 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95169 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95170 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95171 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95195 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95196 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95197 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95202 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95203 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95204 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95205 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95207 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95208 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95211 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95212 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95237 with import type REPLACE_ALL
+
+
+-- Importing creature spawn with guid 95239 with import type REPLACE_ALL
+
+
+COMMIT;
