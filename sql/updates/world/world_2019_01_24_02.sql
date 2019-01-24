3ac66423f86e69dfe1433c86bd713834e89e7612
Kelno <kelnowm@gmail.com>
2019-01-24 14:43:38 +0100

    Fixed #167

---
+++ sql/updates/world/world_2019_01_24_02.sql
@@ -0,0 +1,2 @@
+
+replace into `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `patch_min`, `patch_max`) values('1166485','174794','0','1','-9361.23','532.351','48.6421','1.39092','0','0','0.640737','0.767761','25','0','1','','0','10');
