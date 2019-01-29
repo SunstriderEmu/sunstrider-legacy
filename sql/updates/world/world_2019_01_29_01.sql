1a9067225fca3caf0f7327e3ecc4fadeed6eea38
Kelno <kelnowm@gmail.com>
2019-01-29 19:09:29 +0100

    Cleaned up some old server event spawns

--- sql/updates/world/world_2019_01_29_01.sql
+++ sql/updates/world/world_2019_01_29_01.sql
@@ -1,2 +1,4 @@
-DELETE FROM gameobject WHERE guid IN (1063126, 1063125);
-DELETE FROM creature where spawnID = 1095739;
+UPDATE `quest_template_addon` SET `NextQuestID` = '9826' WHERE (`ID` = '9825');
+UPDATE `quest_template_addon` SET `NextQuestID` = '9826' WHERE (`ID` = '9824');
+UPDATE `quest_template` SET `NextQuestId` = '0' WHERE (`entry` = '9824') and (`patch` = '0');
+UPDATE `quest_template` SET `NextQuestId` = '0' WHERE (`entry` = '9825') and (`patch` = '0');
---
+++ sql/updates/world/world_2019_01_29_02.sql
@@ -0,0 +1,2 @@
+DELETE FROM gameobject WHERE guid IN (1063126, 1063125);
+DELETE FROM creature where spawnID = 1095739;
