2dad6fd5828550333ba569343b0961d2d190b2b6
Kelno <kelnowm@gmail.com>
2019-02-06 20:09:24 +0100

    Fixed sql query

--- sql/updates/world/world_2019_02_06_01.sql
+++ sql/updates/world/world_2019_02_06_01.sql
@@ -1,4 +1,4 @@
 -- fix mage Improved Blizzard talent Rank sequence of freezing effect,Different talent levels should call different freezing effects
-+Replace INTO `spell_ranks` VALUES(12484, 12484, 1);
-+Replace INTO `spell_ranks` VALUES(12484, 12485, 2);
-+Replace INTO `spell_ranks` VALUES(12484, 12486, 3);
+Replace INTO `spell_ranks` VALUES(12484, 12484, 1);
+Replace INTO `spell_ranks` VALUES(12484, 12485, 2);
+Replace INTO `spell_ranks` VALUES(12484, 12486, 3);
