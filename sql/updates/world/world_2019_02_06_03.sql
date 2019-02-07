7d4add3879bf7302dae6a9d9e8dd64325f83100d
Kelno <kelnowm@gmail.com>
2019-02-07 08:34:35 +0100

    Fixed update query

--- sql/updates/world/world_2019_02_06_03.sql
+++ sql/updates/world/world_2019_02_06_03.sql
@@ -1,15 +1,10 @@
-DELETE FROM game_event_gameobject WHERE guid IN (
-	SELECT g.guid
-	FROM gameobject g
-	JOIN gameobject g2 ON g2.position_x < g.position_x + 1 AND g2.position_x > g.position_x - 1 AND g2.position_y < g.position_y + 1 AND g2.position_y > g.position_y - 1 AND g2.position_z < g.position_z + 1 AND g2.position_z > g.position_z - 1 
-	JOIN gameobject_template gt2 ON gt2.entry = g2.id AND gt2.type = 6 AND gt2.data3 = 7897
-	WHERE g.id = 2061
-);
+DELETE gec FROM game_event_gameobject gec 
+JOIN gameobject g ON gec.guid = g.guid
+JOIN gameobject g2 ON g2.position_x < g.position_x + 1 AND g2.position_x > g.position_x - 1 AND g2.position_y < g.position_y + 1 AND g2.position_y > g.position_y - 1 AND g2.position_z < g.position_z + 1 AND g2.position_z > g.position_z - 1 
+JOIN gameobject_template gt2 ON gt2.entry = g2.id AND gt2.type = 6 AND gt2.data3 = 7897
+WHERE g.id = 2061;
 
-DELETE FROM gameobject WHERE guid IN (
-	SELECT g.guid
-	FROM gameobject g
-	JOIN gameobject g2 ON g2.position_x < g.position_x + 1 AND g2.position_x > g.position_x - 1 AND g2.position_y < g.position_y + 1 AND g2.position_y > g.position_y - 1 AND g2.position_z < g.position_z + 1 AND g2.position_z > g.position_z - 1 
-	JOIN gameobject_template gt2 ON gt2.entry = g2.id AND gt2.type = 6 AND gt2.data3 = 7897
-	WHERE g.id = 2061
-);
+DELETE g FROM gameobject g
+JOIN gameobject g2 ON g2.position_x < g.position_x + 1 AND g2.position_x > g.position_x - 1 AND g2.position_y < g.position_y + 1 AND g2.position_y > g.position_y - 1 AND g2.position_z < g.position_z + 1 AND g2.position_z > g.position_z - 1 
+JOIN gameobject_template gt2 ON gt2.entry = g2.id AND gt2.type = 6 AND gt2.data3 = 7897
+WHERE g.id = 2061;
